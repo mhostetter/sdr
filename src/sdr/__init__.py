2a38eb71a9288b94ab18a26e417361bd8e4603ca
mhostetter <matthostetter@gmail.com>
2023-07-06 19:43:16 -0400

    Add `sdr.__version__` variable

--- src/sdr/__init__.py
+++ src/sdr/__init__.py
@@ -1,4 +1,16 @@
 """
 A Python package for software-defined radio (SDR) applications.
 """
+try:
+    from ._version import __version__, __version_tuple__
+except ModuleNotFoundError:  # pragma: no cover
+    import warnings
+
+    __version__ = "0.0.0"
+    __version_tuple__ = (0, 0, 0)
+    warnings.warn(
+        "An error occurred during package install where setuptools_scm failed to create a _version.py file. "
+        "Defaulting version to 0.0.0."
+    )
+
 print("Hello, world")
