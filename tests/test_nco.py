b6da624b21bfcbd4b4dcb480c154891b47c15d48
mhostetter <matthostetter@gmail.com>
2023-07-09 11:50:25 -0400

    Add unit tests for NCO

---
+++ tests/test_nco.py
@@ -0,0 +1,34 @@
+import sdr
+import numpy as np
+
+
+def test_constant_increment():
+    K0 = 1
+    increment = 0.1
+    offset = 0
+    nco = sdr.NCO(K0, increment, offset)
+
+    assert nco.K0 == K0
+    assert nco.increment == increment
+    assert nco.offset == offset
+
+    N = 100
+    y = nco.step(N)
+    y_truth = np.arange(1, N+1) * increment + offset
+    assert np.allclose(y, y_truth)
+
+
+def test_constant_increment_constant_offset():
+    K0 = 1
+    increment = 0.1
+    offset = 0.5
+    nco = sdr.NCO(K0, increment, offset)
+
+    assert nco.K0 == K0
+    assert nco.increment == increment
+    assert nco.offset == offset
+
+    N = 100
+    y = nco.step(N)
+    y_truth = np.arange(1, N+1) * increment + offset
+    assert np.allclose(y, y_truth)
