b28784759686b7c088d416889a33725f20db8df6
mhostetter <matthostetter@gmail.com>
2023-07-11 19:15:32 -0400

    Add `matplotlib` rc params

---
+++ src/sdr/plot/_rc_params.py
@@ -0,0 +1,16 @@
+"""
+A module containing :obj:`sdr`'s default matplotlib rcParams.
+"""
+
+RC_PARAMS = {
+    "figure.figsize": (8, 4),
+    "axes.grid": True,
+    "grid.linestyle": "--",
+    "grid.alpha": 0.8,
+    "figure.max_open_warning": 0,
+    "figure.autolayout": True,
+    "axes.xmargin": 0,
+    "axes.ymargin": 0,
+    "axes.zmargin": 0,
+    "figure.titleweight": "bold",
+}
