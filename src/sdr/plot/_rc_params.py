a5e2442e1dff3f8e1d9f5925c9ddc97c3cfa7792
mhostetter <matthostetter@gmail.com>
2023-07-12 18:41:12 -0400

    Add `sdr.plot.use_style()` function

--- src/sdr/plot/_rc_params.py
+++ src/sdr/plot/_rc_params.py
@@ -1,16 +1,39 @@
 """
 A module containing :obj:`sdr`'s default matplotlib rcParams.
 """
+import matplotlib.pyplot as plt
+
+from .._helper import export
 
 RC_PARAMS = {
-    "figure.figsize": (8, 4),
     "axes.grid": True,
-    "grid.linestyle": "--",
-    "grid.alpha": 0.8,
-    "figure.max_open_warning": 0,
-    "figure.autolayout": True,
     "axes.xmargin": 0,
     "axes.ymargin": 0,
     "axes.zmargin": 0,
+    "figure.autolayout": True,
+    "figure.figsize": (8, 4),
+    "figure.max_open_warning": 0,
     "figure.titleweight": "bold",
+    "grid.alpha": 0.8,
+    "grid.linestyle": "--",
 }
+
+
+@export
+def use_style():
+    """
+    Applies :obj:`sdr`'s default :obj:`matplotlib` rcParams.
+
+    These style settings may be reverted with :func:`matplotlib.pyplot.rcdefaults()`.
+
+    Examples:
+        The following rcParams are applied.
+
+        .. ipython:: python
+
+            sdr.plot._rc_params.RC_PARAMS
+
+    Group:
+        plotting
+    """
+    plt.rcParams.update(RC_PARAMS)
