01915f660a38cadd386d85ab95c170e9c9a52a38
mhostetter <matthostetter@gmail.com>
2023-07-12 19:29:03 -0400

    Add function to plot the step response in `sdr.plot.step_response()`

--- src/sdr/plot/_filter.py
+++ src/sdr/plot/_filter.py
@@ -13,8 +13,10 @@ from ._rc_params import RC_PARAMS
 
 @export
 def impulse_response(b: np.ndarray, a: np.ndarray = 1, N: Optional[int] = None, **kwargs):
-    """
-    Plots the impulse response of a filter.
+    r"""
+    Plots the impulse response $h[n]$ of a filter.
+
+    The impulse response $h[n]$ is the filter output when the input is an impulse $\delta[n]$.
 
     Arguments:
         b: The feedforward coefficients, $b_i$.
@@ -37,10 +39,9 @@ def impulse_response(b: np.ndarray, a: np.ndarray = 1, N: Optional[int] = None, 
 
     if N is None:
         if a.size == 1 and a[0] == 1:
-            # FIR filter
-            N = b.size
+            N = b.size  # FIR filter
         else:
-            N = 100
+            N = 100  # IIR filter
 
     # Delta impulse function
     d = np.zeros(N, dtype=np.float32)
@@ -70,3 +71,64 @@ def impulse_response(b: np.ndarray, a: np.ndarray = 1, N: Optional[int] = None, 
         plt.xlabel("Sample")
         plt.ylabel("Amplitude")
         plt.title("Impulse Response, $h[n]$")
+
+
+@export
+def step_response(b: np.ndarray, a: np.ndarray = 1, N: Optional[int] = None, **kwargs):
+    r"""
+    Plots the step response $s[n]$ of a filter.
+
+    The step response $s[n]$ is the filter output when the input is a unit step $u[n]$.
+
+    Arguments:
+        b: The feedforward coefficients, $b_i$.
+        a: The feedback coefficients, $a_j$. For FIR filters, this is set to 1.
+        N: The number of samples to plot. If `None`, the length of `b` is used for FIR filters and
+            100 for IIR filters.
+        **kwargs: Additional keyword arguments to pass to :func:`matplotlib.pyplot.plot()`.
+
+    See Also:
+        sdr.IIR
+
+    Examples:
+        See the :ref:`iir-filter` example.
+
+    Group:
+        plotting
+    """
+    b = np.atleast_1d(b)
+    a = np.atleast_1d(a)
+
+    if N is None:
+        if a.size == 1 and a[0] == 1:
+            N = b.size  # FIR filter
+        else:
+            N = 100  # IIR filter
+
+    # Unit step function
+    u = np.ones(N, dtype=np.float32)
+
+    # Filter the impulse
+    zi = scipy.signal.lfiltic(b, a, y=[], x=[])
+    s, zi = scipy.signal.lfilter(b, a, u, zi=zi)
+
+    with plt.rc_context(RC_PARAMS):
+        label = kwargs.pop("label", None)
+        if np.iscomplexobj(s):
+            if label is None:
+                label = "real"
+                label2 = "imag"
+            else:
+                label = label + " (real)"
+                label2 = label + " (imag)"
+            plt.plot(np.arange(s.size), s.real, label=label, **kwargs)
+            plt.plot(np.arange(s.size), s.imag, label=label2, **kwargs)
+        else:
+            plt.plot(np.arange(s.size), s, label=label, **kwargs)
+
+        if label:
+            plt.legend()
+
+        plt.xlabel("Sample")
+        plt.ylabel("Amplitude")
+        plt.title("Step Response, $s[n]$")
