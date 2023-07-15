88dd7c3f7a9910b0b03164f80333e167390b27a0
mhostetter <matthostetter@gmail.com>
2023-07-15 12:03:04 -0400

    Add `sdr.hexdump()` function

--- src/sdr/_data.py
+++ src/sdr/_data.py
@@ -112,6 +112,63 @@ def unpack(x: npt.ArrayLike, bpe: int, dtype: npt.DTypeLike | None = None) -> np
     return y.astype(dtype)
 
 
+@export
+def hexdump(data: npt.ArrayLike | bytes, width: int = 16) -> str:
+    """
+    Returns a hexdump of the specified data.
+
+    Arguments:
+        data: The data to display. Each element is considered one byte. Use :func:`sdr.pack()`
+            or :func:`sdr.unpack()` to convert data with variable bits per element.
+        width: The number of bytes per line.
+
+    Returns:
+        A string containing the hexdump of the data.
+
+    Examples:
+        .. ipython:: python
+
+            print(sdr.hexdump(b"The quick brown fox jumps over the lazy dog"))
+
+        .. ipython:: python
+
+            print(sdr.hexdump([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], width=4))
+
+    Group:
+        data
+    """
+    if isinstance(data, bytes):
+        data = np.frombuffer(data, dtype=np.uint8)
+    else:
+        data = np.asarray(data)
+
+    if not np.issubdtype(data.dtype, np.integer):
+        raise ValueError("Argument 'data' must be an integer array, not have {data.dtype} dtype.")
+    if not data.ndim == 1:
+        raise ValueError("Argument 'data' must be a 1D array, not have {data.ndim} dimensions.")
+    if not np.count_nonzero(data < 0) == 0:
+        raise ValueError("Argument 'data' must contain only positive values.")
+    if not np.count_nonzero(data > 255) == 0:
+        raise ValueError("Argument 'data' must contain only values less than 256.")
+    if not 1 <= width <= 16:
+        raise ValueError("Argument 'width' must be between 1 and 16, not {width}.")
+
+    if width > data.size:
+        width = data.size
+
+    string = ""
+    for i in range(0, data.size, width):
+        line = data[i : i + width]
+        string += f"{i:08x}  "
+        string += " ".join(f"{x:02x}" for x in line)
+        string += "   " * (width - line.size)
+        string += "  "
+        string += "".join(chr(x) if 32 <= x <= 126 else "." for x in line)
+        string += "\n"
+
+    return string
+
+
 def _unsigned_dtype(bits: int) -> np.integer:
     """
     Returns the smallest unsigned integer dtype that can hold the specified number of bits.
