37cd80294f93fbd3a8c30347be0709d47a2b7ed4
mhostetter <matthostetter@gmail.com>
2023-07-16 17:23:48 -0400

    Fix bug in labels for time-domain plot

--- src/sdr/plot/_time_domain.py
+++ src/sdr/plot/_time_domain.py
@@ -70,11 +70,9 @@ def time_domain(x: npt.ArrayLike, sample_rate: float = 1.0, **kwargs):
         label = kwargs.pop("label", None)
         if np.iscomplexobj(x):
             if label is None:
-                label = "real"
-                label2 = "imag"
+                label, label2 = "real", "imag"
             else:
-                label = label + " (real)"
-                label2 = label + " (imag)"
+                label, label2 = label + " (real)", label + " (imag)"
             plt.plot(t, x.real, label=label, **kwargs)
             plt.plot(t, x.imag, label=label2, **kwargs)
         else:
