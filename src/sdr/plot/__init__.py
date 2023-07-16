4e9f28e897daf5adafdc7350f0b857f18b49c754
mhostetter <matthostetter@gmail.com>
2023-07-16 11:48:39 -0400

    Add function to plot periodogram in `sdr.plot.periodogram()`

--- src/sdr/plot/__init__.py
+++ src/sdr/plot/__init__.py
@@ -2,5 +2,6 @@
 A subpackage for various plotting functions using :obj:`matplotlib`.
 """
 from ._filter import *  # pylint: disable=redefined-builtin
+from ._freq_domain import *
 from ._rc_params import *
 from ._time_domain import *
