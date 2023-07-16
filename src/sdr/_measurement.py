46457dcaa23c0d0b5947aafbd8f12521e01633f1
mhostetter <matthostetter@gmail.com>
2023-07-15 20:09:59 -0400

    Add `sdr.crest_factor()`

--- src/sdr/_measurement.py
+++ src/sdr/_measurement.py
@@ -10,7 +10,7 @@ from ._helper import export
 
 
 @export
-def papr(x: npt.ArrayLike):
+def papr(x: npt.ArrayLike) -> float:
     """
     Measures the peak-to-average power ratio (PAPR) of a signal.
 
@@ -26,7 +26,29 @@ def papr(x: npt.ArrayLike):
     x = np.asarray(x)
 
     mag2 = np.abs(x) ** 2
-    peak = np.max(mag2)
-    average = np.mean(mag2)
+    peak_power = np.max(mag2)
+    average_power = np.mean(mag2)
 
-    return 10 * np.log10(peak / average)
+    return 10 * np.log10(peak_power / average_power)
+
+
+@export
+def crest_factor(x: npt.ArrayLike) -> float:
+    """
+    Measures the crest factor of a signal.
+
+    Arguments:
+        x: The time-domain signal $x[n]$ to measure.
+
+    Returns:
+        The crest factor of the signal $x[n]$.
+
+    Group:
+        measurement
+    """
+    x = np.asarray(x)
+
+    peak_voltage = np.max(np.abs(x))
+    rms_voltage = np.sqrt(np.mean(np.abs(x) ** 2))
+
+    return peak_voltage / rms_voltage
