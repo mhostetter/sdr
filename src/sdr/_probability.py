99ae02ec3753e70cfe9acfffa2566fa493096cc6
mhostetter <matthostetter@gmail.com>
2023-07-16 19:22:12 -0400

    Add `sdr.Qinv()`

--- src/sdr/_probability.py
+++ src/sdr/_probability.py
@@ -12,14 +12,15 @@ from ._helper import export
 
 @export
 def Q(x: npt.ArrayLike) -> np.ndarray:
-    """
-    Computes the complementary cumulative distribution function $Q(x)$ of the standard normal distribution.
+    r"""
+    Computes the complementary cumulative distribution function $Q(x)$
+    of the standard normal distribution $\mathcal{N}(0, 1)$.
 
     Arguments:
         x: The real-valued input $x$.
 
     Returns:
-        The probability $p$ that $x$ is exceeded by samples drawn from \mathcal{N}(0, 1)$.
+        The probability $p$ that $x$ is exceeded.
 
     Group:
         probability
@@ -27,3 +28,23 @@ def Q(x: npt.ArrayLike) -> np.ndarray:
     x = np.asarray(x)
     p = scipy.special.erfc(x / np.sqrt(2)) / 2  # pylint: disable=no-member
     return p
+
+
+@export
+def Qinv(p: npt.ArrayLike) -> np.ndarray:
+    r"""
+    Computes the inverse complementary cumulative distribution function $Q^{-1}(p)$
+    of the standard normal distribution $\mathcal{N}(0, 1)$.
+
+    Arguments:
+        p: The probability $p$ of exceeding the returned value $x$.
+
+    Returns:
+        The real-valued $x$ that is exceeded with probability $p$.
+
+    Group:
+        probability
+    """
+    p = np.asarray(p)
+    x = np.sqrt(2) * scipy.special.erfcinv(2 * p)  # pylint: disable=no-member
+    return x
