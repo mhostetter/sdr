d181352cb868b8cfacbe045b34b8371811610e26
mhostetter <matthostetter@gmail.com>
2023-07-18 22:56:20 -0400

    Add unit tests for BSC

---
+++ tests/chnnael_models/test_bsc.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pytest
+
+import sdr
+
+
+def test_exceptions():
+    with pytest.raises(ValueError):
+        # p must be between 0 and 1
+        sdr.bsc([0, 1], -0.1)
+    with pytest.raises(ValueError):
+        # p must be between 0 and 1
+        sdr.bsc([0, 1], 1.1)
+
+
+def test_types():
+    y = sdr.bsc(0, 0.5)
+    assert isinstance(y, int)
+
+    y = sdr.bsc([0, 1], 0.5)
+    assert isinstance(y, np.ndarray)
+
+
+@pytest.mark.parametrize("p", [0.1, 0.2, 0.3, 0.4, 0.5])
+def test_bit_flips(p):
+    N = int(1000 / p)
+    x = np.random.randint(0, 2, N)
+    y = sdr.bsc(x, p)
+    assert np.count_nonzero(x != y) / N == pytest.approx(p, rel=1e-1)
