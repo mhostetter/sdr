c1b9803778d601033cf11c37fea7cb7c3d90127d
mhostetter <matthostetter@gmail.com>
2023-07-19 11:38:04 -0400

    Add the discrete memoryless channel in `sdr.dmc()`

--- src/sdr/_channel_model.py
+++ src/sdr/_channel_model.py
@@ -78,3 +78,77 @@ def bec(x: npt.ArrayLike, p: float) -> np.ndarray:
     y = np.where(np.random.rand(*x.shape) < p, -1, x)
 
     return y if y.ndim > 0 else y.item()
+
+
+@export
+def dmc(
+    x: npt.ArrayLike, P: npt.ArrayLike, X: npt.ArrayLike | None = None, Y: npt.ArrayLike | None = None
+) -> np.ndarray:
+    r"""
+    Passes the input sequence $x$ through a discrete memoryless channel (DMC) with transition
+    probability matrix $P$.
+
+    Arguments:
+        x: The input sequence $x$ with $x_i \in \mathcal{X}$.
+        P: The $m \times n$ transition probability matrix $P$, where $P_{i,j} = \Pr(Y = y_j | X = x_i)$.
+        X: The input alphabet $\mathcal{X}$ of size $m$. If `None`, it is assumed that
+            $\mathcal{X} = \{0, 1, \ldots, m-1\}$.
+        Y: The output alphabet $\mathcal{Y}$ of size $n$. If `None`, it is assumed that
+            $\mathcal{Y} = \{0, 1, \ldots, n-1\}$.
+
+    Returns:
+        The output sequence $y$ with $y_i \in \mathcal{Y}$.
+
+    Examples:
+        Define the binary symmetric channel (BSC) with transition probability $p=0.25$.
+
+        .. ipython:: python
+
+            p = 0.25
+            x = np.random.randint(0, 2, 20); x
+            P = [[1 - p, p], [p, 1 - p]]
+            y = sdr.dmc(x, P); y
+            x == y
+
+        Define the binary erasure channel (BEC) with erasure probability $p=0.25$.
+
+        .. ipython:: python
+
+            p = 0.25
+            x = np.random.randint(0, 2, 20); x
+            P = [[1 - p, 0, p], [0, 1 - p, p]]
+            # Specify the erasure as -1
+            y = sdr.dmc(x, P, Y=[0, 1, -1]); y
+            x == y
+
+    Group:
+        channel-model
+    """
+    x = np.asarray(x)
+
+    P = np.asarray(P)
+    if not P.ndim == 2:
+        raise ValueError(f"Argument 'P' must be a 2D array, not {P.ndim}D.")
+    if not np.all(P >= 0):
+        raise ValueError(f"Argument 'P' must have non-negative entires, not {P}.")
+    if not np.allclose(P.sum(axis=1), 1):
+        raise ValueError(f"Argument 'P' must have row that sum to 1, not {P.sum(axis=1)}.")
+
+    X = np.asarray(X) if X is not None else np.arange(P.shape[0])
+    if not X.ndim == 1:
+        raise ValueError(f"Argument 'X' must be a 1D array, not {X.ndim}D.")
+    if not P.shape[0] == X.shape[0]:
+        raise ValueError(f"Argument 'P' must have {X.shape[0]} rows, not {P.shape[0]}.")
+
+    Y = np.asarray(Y) if Y is not None else np.arange(P.shape[1])
+    if not Y.ndim == 1:
+        raise ValueError(f"Argument 'Y' must be a 1D array, not {Y.ndim}D.")
+    if not P.shape[1] == Y.shape[0]:
+        raise ValueError(f"Argument 'P' must have {Y.shape[0]} columns, not {P.shape[1]}.")
+
+    y = np.zeros_like(x)
+    for i, Xi in enumerate(X):
+        idxs = np.where(x == Xi)[0]
+        y[idxs] = np.random.choice(Y, size=idxs.shape, p=P[i])
+
+    return y if y.ndim > 0 else y.item()
