8faf8d628a30c1a74ec71b3e94d570148384a1e9
mhostetter <matthostetter@gmail.com>
2023-07-18 23:58:44 -0400

    Add a binary erasure channel in `sdr.bec()`

--- src/sdr/_channel_model.py
+++ src/sdr/_channel_model.py
@@ -11,25 +11,26 @@ from ._helper import export
 
 @export
 def bsc(x: npt.ArrayLike, p: float) -> np.ndarray:
-    """
+    r"""
     Passes the binary input sequence $x$ through a binary symmetric channel (BSC)
     with transition probability $p$.
 
-    Args:
-        x: The input sequence $x$.
+    Arguments:
+        x: The input sequence $x \in \{0, 1\}$.
         p: The probability $p$ of a bit flip.
 
     Returns:
-        The output sequence $y$.
+        The output sequence $y \in \{0, 1\}$.
 
     Examples:
-        A BSC with transition probability $p=0.5$ is equivalent to a random bit
-        flip. For example, when 20 zeros are inputted, roughly 10 ones should
-        appear in the output.
+        When 20 bits are passed through a BSC with transition probability $p=0.25$,
+        roughly 5 bits should be flipped at the output.
 
         .. ipython:: python
 
-            sdr.bsc([0]*20, 0.5)
+            x = np.random.randint(0, 2, 20); x
+            y = sdr.bsc(x, 0.25); y
+            x == y
 
     Group:
         channel-model
@@ -40,5 +41,40 @@ def bsc(x: npt.ArrayLike, p: float) -> np.ndarray:
     x = np.asarray(x)
     flip = np.random.choice([0, 1], size=x.shape, p=[1 - p, p])
     y = x ^ flip
+
+    return y if y.ndim > 0 else y.item()
+
+
+@export
+def bec(x: npt.ArrayLike, p: float) -> np.ndarray:
+    r"""
+    Passes the binary input sequence $x$ through a binary erasure channel (BEC)
+    with erasure probability $p$.
+
+    Arguments:
+        x: The input sequence $x \in \{0, 1\}$.
+        p: The probability $p$ of a bit erasure.
+
+    Returns:
+        The output sequence $y \in \{0, 1, e\}$. Erasures $e$ are represented by -1.
+
+    Examples:
+        When 20 bits are passed through a BEC with erasure probability $p=0.25$,
+        roughly 5 bits should be erased at the output.
+
+        .. ipython:: python
+
+            x = np.random.randint(0, 2, 20); x
+            y = sdr.bec(x, 0.25); y
+            x == y
+
+    Group:
+        channel-model
+    """
+    if not 0 <= p <= 1:
+        raise ValueError(f"Argument 'p' must be between 0 and 1, not {p}.")
+
+    x = np.asarray(x)
+    y = np.where(np.random.rand(*x.shape) < p, -1, x)
 
     return y if y.ndim > 0 else y.item()
