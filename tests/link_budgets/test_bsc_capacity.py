2b7ca027304d6fd40623b16b437628103c5d9576
mhostetter <matthostetter@gmail.com>
2023-07-20 01:33:21 -0400

    Add unit tests for BSC capacity

---
+++ tests/link_budgets/test_bsc_capacity.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pytest
+
+import sdr
+
+
+def test_exceptions():
+    with pytest.raises(ValueError):
+        # Transition probability must be between 0 and 1
+        sdr.bsc_capacity(-0.1)
+    with pytest.raises(ValueError):
+        # Transition probability must be between 0 and 1
+        sdr.bsc_capacity(1.1)
+
+
+def test_limit_cases():
+    assert sdr.bsc_capacity(0) == 1
+    assert sdr.bsc_capacity(1) == 1
+
+
+def test_values():
+    C = sdr.bsc_capacity([0, 0.5, 1])
+    assert isinstance(C, np.ndarray)
+    assert np.array_equal(C, [1, 0, 1])
+
+    C = sdr.bsc_capacity(0.5)
+    assert isinstance(C, float)
+    assert C == 0
