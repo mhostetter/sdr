312ff594c64991b60dbcd8d43e4fa820084d4431
mhostetter <matthostetter@gmail.com>
2023-07-21 23:02:43 -0400

    Add `sdr.parabolic_antenna()`

--- src/sdr/_link_budget.py
+++ src/sdr/_link_budget.py
@@ -53,3 +53,64 @@ def fspl(d: npt.ArrayLike, f: npt.ArrayLike) -> npt.ArrayLike:
     f = np.asarray(f)
     loss = 20 * np.log10(4 * np.pi * d * f / SPEED_OF_LIGHT)
     return loss
+
+
+@export
+def parabolic_antenna(
+    freq: float,
+    diameter: float,
+    efficiency: float = 0.55,
+) -> tuple[npt.ArrayLike, npt.ArrayLike]:
+    r"""
+    Calculates the gain $G$ and beamwidth $\theta$ of a parabolic reflector.
+
+    Arguments:
+        freq: The frequency $f$ in Hz of the signal.
+        diameter: The diameter $d$ in meters of the parabolic reflector.
+        efficiency: The efficiency $0 \le \eta \le 1$ of the parabolic reflector.
+
+    Returns:
+        - The gain $G$ in dBi.
+        - The half-power beamwidth $\theta$ in degrees.
+
+    Notes:
+        $$G = \left( \frac{\pi d f}{c} \right)^2 \eta$$
+
+        $$\theta = \arcsin \left( \frac{3.83 c}{\pi d f} \right)$$
+
+    Examples:
+        A 1 meter dish at 1 GHz with 55% efficiency has a gain of 17.8 dBi and a 3-dB beamwidth of 21.4 degrees.
+
+        .. ipython:: python
+
+            sdr.parabolic_antenna(1e9, 1)
+
+        A 2 meter dish at 1 GHz with 55% efficiency has a gain of 23.8 dBi and a 3-dB beamwidth of 10.5 degrees.
+        Since the antenna gain is proportional to the square of the reflector diameter, doubling the diameter
+        results in a 6 dB increase in the gain, which we observe.
+
+        .. ipython:: python
+
+            sdr.parabolic_antenna(1e9, 2)
+
+    Group:
+        link-budget
+    """
+    freq = np.asarray(freq)
+
+    diameter = np.asarray(diameter)
+    if not np.all(diameter > 0):
+        raise ValueError("Argument 'diameter' must be positive.")
+
+    efficiency = np.asarray(efficiency)
+    if not np.all((0 <= efficiency) & (efficiency <= 1)):
+        raise ValueError("Argument 'efficiency' must be between 0 and 1.")
+
+    lambda_ = SPEED_OF_LIGHT / freq  # Wavelength in meters
+    G = (np.pi * diameter / lambda_) ** 2 * efficiency  # Gain in linear units
+    G = 10 * np.log10(G)  # Gain in dBi
+
+    theta = np.arcsin(3.83 * lambda_ / (np.pi * diameter))  # Beamwidth in radians
+    theta = np.rad2deg(theta)  # Beamwidth in degrees
+
+    return G, theta
