560362352e54766903eca1b45aafea6ec4ff910b
mhostetter <matthostetter@gmail.com>
2023-07-23 12:39:03 -0400

    Add unit tests for binary codes

--- tests/modulation/test_binary_code.py
+++ tests/modulation/test_binary_code.py
@@ -11,28 +11,28 @@ def test_exceptions():
 
 
 def test_1():
-    code = sdr.gray_code(1)
+    code = sdr.binary_code(1)
     code_truth = [0, 1]
     assert isinstance(code, np.ndarray)
     assert np.array_equal(code, code_truth)
 
 
 def test_2():
-    code = sdr.gray_code(2)
-    code_truth = [0, 1, 3, 2]
+    code = sdr.binary_code(2)
+    code_truth = [0, 1, 2, 3]
     assert isinstance(code, np.ndarray)
     assert np.array_equal(code, code_truth)
 
 
 def test_3():
-    code = sdr.gray_code(3)
-    code_truth = [0, 1, 3, 2, 6, 7, 5, 4]
+    code = sdr.binary_code(3)
+    code_truth = [0, 1, 2, 3, 4, 5, 6, 7]
     assert isinstance(code, np.ndarray)
     assert np.array_equal(code, code_truth)
 
 
 def test_4():
-    code = sdr.gray_code(4)
-    code_truth = [0, 1, 3, 2, 6, 7, 5, 4, 12, 13, 15, 14, 10, 11, 9, 8]
+    code = sdr.binary_code(4)
+    code_truth = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     assert isinstance(code, np.ndarray)
     assert np.array_equal(code, code_truth)
