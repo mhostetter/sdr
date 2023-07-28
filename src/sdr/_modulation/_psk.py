6305076d54e688984855b9c84367d3ea1167e013
mhostetter <matthostetter@gmail.com>
2023-07-27 21:21:55 -0400

    Add theoretical BER/SER to PSK

--- src/sdr/_modulation/_psk.py
+++ src/sdr/_modulation/_psk.py
@@ -8,6 +8,7 @@ import numpy.typing as npt
 from typing_extensions import Literal
 
 from .._helper import export
+from .._probability import Q
 from ._symbol_mapping import binary_code, gray_code
 
 
@@ -93,6 +94,79 @@ class PSK:
         error_vectors = np.subtract.outer(x_hat, self.symbol_map)
         s_hat = np.argmin(np.abs(error_vectors), axis=-1)
         return s_hat
+
+    def bit_error_rate(self, ebn0: npt.ArrayLike | None = None, esn0: npt.ArrayLike | None = None) -> np.ndarray:
+        r"""
+        Computes the bit error rate (BER) at the provided SNRs.
+
+        Arguments:
+            ebn0: Bit energy $E_b$ to noise PSD $N_0$ ratio in dB. If `None`, `esn0` must be provided.
+            esn0: Symbol energy $E_s$ to noise PSD $N_0$ ratio in dB. If `None`, `ebn0` must be provided.
+
+        Returns:
+            The bit error rate $P_b$.
+
+        References:
+            - John Proakis, *Digital Communications*, Chapter 4: Optimum Receivers for AWGN Channels.
+        """
+        M = self.order  # Modulation order
+        if ebn0 is not None:
+            ebn0 = np.asarray(ebn0)
+        elif esn0 is not None:
+            esn0 = np.asarray(esn0)
+            ebn0 = esn0 - 10 * np.log10(M)  # Bit energy to noise PSD ratio
+        else:
+            raise ValueError("Argument 'ebn0' or 'esn0' must be provided.")
+
+        ebn0_linear = 10 ** (ebn0 / 10)
+
+        if M in [2, 4]:
+            # Equation 4.3-13
+            Pe = Q(np.sqrt(2 * ebn0_linear))
+        else:
+            # Equation 4.3-20
+            k = np.log2(M)  # Bits per symbol
+            Pe = self.symbol_error_rate(ebn0, esn0) / k
+
+        return Pe
+
+    def symbol_error_rate(self, ebn0: npt.ArrayLike | None = None, esn0: npt.ArrayLike | None = None) -> np.ndarray:
+        r"""
+        Computes the symbol error rate (SER) at the provided SNRs.
+
+        Arguments:
+            esn0: Symbol energy $E_s$ to noise PSD $N_0$ ratio in dB. If `None`, `ebn0` must be provided.
+            ebn0: Bit energy $E_b$ to noise PSD $N_0$ ratio in dB. If `None`, `esn0` must be provided.
+
+        Returns:
+            The symbol error rate $P_e$.
+
+        References:
+            - John Proakis, *Digital Communications*, Chapter 4: Optimum Receivers for AWGN Channels.
+        """
+        M = self.order  # Modulation order
+        if ebn0 is not None:
+            ebn0 = np.asarray(ebn0)
+        elif esn0 is not None:
+            esn0 = np.asarray(esn0)
+            ebn0 = esn0 - 10 * np.log10(M)  # Bit energy to noise PSD ratio
+        else:
+            raise ValueError("Argument 'ebn0' or 'esn0' must be provided.")
+
+        ebn0_linear = 10 ** (ebn0 / 10)
+
+        if M == 2:
+            # Equation 4.3-13
+            Pe = Q(np.sqrt(2 * ebn0_linear))
+        elif M == 4:
+            # Equation 4.3-15
+            Pe = 2 * Q(np.sqrt(2 * ebn0_linear)) * (1 - 1 / 2 * Q(np.sqrt(2 * ebn0_linear)))
+        else:
+            # Equation 4.3-17
+            # NOTE: This is an approximation for large M
+            Pe = 2 * Q(np.sqrt(2 * np.log2(M) * np.sin(np.pi / M) ** 2 * ebn0_linear))
+
+        return Pe
 
     @property
     def order(self) -> int:
