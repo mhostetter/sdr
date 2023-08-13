a7278c2a188a82058e1186df7fb1f271789284ef
mhostetter <matthostetter@gmail.com>
2023-08-13 14:46:51 -0400

    Test FIR streaming mode equals full mode

--- tests/dsp/test_fir.py
+++ tests/dsp/test_fir.py
@@ -37,6 +37,24 @@ def test_streaming():
     assert np.allclose(y, y_truth)
 
 
+def test_streaming_match_full():
+    N = 50
+    x = np.random.randn(N) + 1j * np.random.randn(N)  # Input signal
+    h = np.random.randn(10) + 1j * np.random.randn(10)  # FIR impulse response
+
+    fir1 = sdr.FIR(h)
+    y_full = fir1(x, mode="full")
+
+    fir2 = sdr.FIR(h, streaming=True)
+    d = 10  # Stride
+    y_stream = np.zeros_like(y_full)
+    for i in range(0, N, d):
+        y_stream[i : i + d] = fir2(x[i : i + d])
+    y_stream[i + d :] = fir2.flush()
+
+    np.testing.assert_array_almost_equal(y_full, y_stream)
+
+
 def test_impulse_response():
     h_truth = np.random.randn(10) + 1j * np.random.randn(10)  # FIR impulse response
     fir = sdr.FIR(h_truth)
