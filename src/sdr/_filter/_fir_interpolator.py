66372abf68b0b0da3c26ae5b2ce951ff9fb1c9ea
mhostetter <matthostetter@gmail.com>
2023-08-12 20:33:00 -0400

    Fix typos in `Interpolator`

--- src/sdr/_filter/_fir_interpolator.py
+++ src/sdr/_filter/_fir_interpolator.py
@@ -184,7 +184,7 @@ class Interpolator(FIR):
             @savefig sdr_fir_interpolator_3.png
             plt.figure(figsize=(8, 4)); \
             sdr.plot.time_domain(x, marker="o", label="Input"); \
-            sdr.plot.time_domain(y, sample_rate=fir.rate, marker=".", label="Filtered"); \
+            sdr.plot.time_domain(y, sample_rate=fir.rate, offset=-fir.delay/fir.rate, marker=".", label="Filtered"); \
             plt.title("Interpolation by 7 with the zero-order hold method"); \
             plt.tight_layout();
 
@@ -292,8 +292,8 @@ class Interpolator(FIR):
 
                 - `"rate"`: The output signal $y[n]$ has length $L r$ proportional to the interpolation rate $r$.
                   Output sample 0 aligns with input sample 0.
-                - `"full"`: The full convolution is performed. The output signal $y[n]$ has length $L r + N - r$,
-                  where $L$ is the length of the multirate filter. Output sample :obj:`~sdr.Interpolator.delay` aligns
+                - `"full"`: The full convolution is performed. The output signal $y[n]$ has length $(L + N) r$,
+                  where $N$ is the order of the multirate filter. Output sample :obj:`~sdr.Interpolator.delay` aligns
                   with input sample 0.
 
                 In streaming mode, the `"full"` convolution is performed. However, for each $L$ input samples
