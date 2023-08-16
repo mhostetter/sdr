abeda571c7d36dc0e425703178c2b61f3f4378a6
mhostetter <matthostetter@gmail.com>
2023-08-16 13:53:37 -0400

    Add an example plot to `fspl()`

--- src/sdr/_link_budget/_path_loss.py
+++ src/sdr/_link_budget/_path_loss.py
@@ -24,6 +24,11 @@ def fspl(d: npt.ArrayLike, f: npt.ArrayLike) -> np.ndarray:
     Returns:
         The free-space path loss (FSPL) in dB.
 
+    Note:
+        The free-space path loss equation is only valid in the far field. For $d < \lambda / 4 \pi$, the FSPL
+        equation has a negative result, implying a path gain. This is not possible. So these path losses
+        are set to 0 dB.
+
     Examples:
         Compute the free-space path loss for a 1 km link at 1 GHz.
 
@@ -44,6 +49,21 @@ def fspl(d: npt.ArrayLike, f: npt.ArrayLike) -> np.ndarray:
         .. ipython:: python
 
             sdr.fspl(1e3, 2e9)
+
+        Plot the free-space path loss at 1 GHz for distances up to 1 km.
+
+        .. ipython:: python
+
+            d = np.linspace(0, 1_000, 1000)  # m
+            fspl = sdr.fspl(d, 1e9)  # dB
+
+            @savefig sdr_fspl_1.png
+            plt.figure(figsize=(8, 4)); \
+            plt.plot(d, fspl); \
+            plt.xlabel('Distance (m)'); \
+            plt.ylabel('Free-space path loss (dB)'); \
+            plt.grid(True); \
+            plt.tight_layout()
 
     Group:
         link-budget-path-losses
