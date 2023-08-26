2be7aa960b9e91cef840b13161fec5751e393f92
mhostetter <matthostetter@gmail.com>
2023-08-26 14:43:19 -0400

    Add unit tests for half-sine pulse shapes

--- tests/modulation/test_half_sine.py
+++ tests/modulation/test_half_sine.py
@@ -6,31 +6,31 @@ import sdr
 
 def test_exceptions():
     with pytest.raises(TypeError):
-        sdr.rectangular(8.0)
+        sdr.half_sine(8.0)
     with pytest.raises(TypeError):
-        sdr.rectangular(8, span=1.0)
+        sdr.half_sine(8, span=1.0)
 
     with pytest.raises(ValueError):
         # Need at least 1 samples per symbol
-        sdr.rectangular(0)
+        sdr.half_sine(0)
     with pytest.raises(ValueError):
         # Need at least 1 symbol
-        sdr.rectangular(8, span=0)
+        sdr.half_sine(8, span=0)
 
 
 def test_4():
-    h = sdr.rectangular(4)
-    h_truth = 0.5 * np.ones(4)
-    assert np.array_equal(h, h_truth)
+    h = sdr.half_sine(4)
+    h_truth = np.sqrt(2) / 2 * np.sin(np.pi * np.arange(4) / 4)
+    np.testing.assert_array_almost_equal(h, h_truth)
 
 
 def test_4_3():
-    h = sdr.rectangular(4, span=3)
+    h = sdr.half_sine(4, span=3)
     h_truth = np.concatenate(
         (
             np.zeros(4),
-            0.5 * np.ones(4),
+            np.sqrt(2) / 2 * np.sin(np.pi * np.arange(4) / 4),
             np.zeros(4),
         )
     )
-    assert np.array_equal(h, h_truth)
+    np.testing.assert_array_almost_equal(h, h_truth)
