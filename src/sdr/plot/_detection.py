e0f083fdf66eb09900fbca74d1b8c5b5d6486277
mhostetter <matthostetter@gmail.com>
2023-09-02 10:40:10 -0400

    Rename variables

--- src/sdr/plot/_detection.py
+++ src/sdr/plot/_detection.py
@@ -13,8 +13,8 @@ from ._rc_params import RC_PARAMS
 
 @export
 def roc(
-    P_fa: npt.ArrayLike,
-    P_d: npt.ArrayLike,
+    p_fa: npt.ArrayLike,
+    p_d: npt.ArrayLike,
     type: Literal["linear", "semilogx", "semilogy", "loglog"] = "semilogx",  # pylint: disable=redefined-builtin
     **kwargs,
 ):
@@ -22,8 +22,8 @@ def roc(
     Plots the receiver operating characteristic (ROC) curve as a function of $P_{FA}$.
 
     Arguments:
-        P_fa: The probability of false alarm $P_{FA}$.
-        P_d: The probability of detection $P_D$.
+        p_fa: The probability of false alarm $P_{FA}$.
+        p_d: The probability of detection $P_D$.
         type: The type of plot to generate.
         kwargs: Additional keyword arguments to pass to the plotting function defined by `type`.
 
@@ -35,13 +35,13 @@ def roc(
         kwargs = {**default_kwargs, **kwargs}
 
         if type == "linear":
-            plt.plot(P_fa, P_d, **kwargs)
+            plt.plot(p_fa, p_d, **kwargs)
         elif type == "semilogx":
-            plt.semilogx(P_fa, P_d, **kwargs)
+            plt.semilogx(p_fa, p_d, **kwargs)
         elif type == "semilogy":
-            plt.semilogy(P_fa, P_d, **kwargs)
+            plt.semilogy(p_fa, p_d, **kwargs)
         elif type == "loglog":
-            plt.loglog(P_fa, P_d, **kwargs)
+            plt.loglog(p_fa, p_d, **kwargs)
         else:
             raise ValueError(
                 f"Argument 'type' must be one of ['linear', 'semilogx', 'semilogy', 'loglog'], not {type!r}."
