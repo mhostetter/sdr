0702cb0192d8e3648d1ec28ef7770f6d9d3225bb
mhostetter <matthostetter@gmail.com>
2023-11-12 13:17:29 -0500

    Add `Resampler` polyphase rational resampler
    Fixes Add `sdr.MultirateFIR` #33

--- src/sdr/_filter/_polyphase.py
+++ src/sdr/_filter/_polyphase.py
@@ -25,7 +25,7 @@ def multirate_taps(
     Computes the multirate FIR filter that achieves rational resampling by $P/Q$.
 
     Note:
-        This filter can be used with :class:`sdr.Interpolator` or :class:`sdr.Decimator`.
+        This filter can be used with :class:`sdr.Interpolator`, :class:`sdr.Decimator`, or :class:`sdr.Resampler`.
 
     Arguments:
         P: The interpolation rate $P$.
@@ -915,3 +915,328 @@ class Decimator(FIR):
                 fir.polyphase_taps
         """
         return self._polyphase_taps
+
+
+@export
+class Resampler(FIR):
+    r"""
+    Implements a polyphase rational resampling finite impulse response (FIR) filter.
+
+    Notes:
+        The polyphase rational resampling filter is equivalent to first upsampling the input signal $x[n]$ by $P$
+        (by inserting $P-1$ zeros between each sample), filtering the upsampled signal with the prototype FIR filter
+        with feedforward coefficients $h_{i}$, and then decimating the filtered signal by $Q$ (by discarding $Q-1$
+        samples between each sample).
+
+        Instead, the polyphase rational resampling filter first decomposes the prototype FIR filter into $P$ polyphase
+        filters with feedforward coefficients $h_{i, j}$. The polyphase filters are then applied to the
+        input signal $x[n]$ in parallel. The output of the polyphase filters are then commutated by $Q$ to produce
+        the output signal $y[n]$. This prevents the need to multiply with zeros in the upsampled input,
+        as is needed in the first case.
+
+        .. code-block:: text
+           :caption: Polyphase 2/3 Resampling FIR Filter Block Diagram
+
+                                  +------------------------+
+                              +-->| h[0], h[2], h[4], h[6] |--> ..., ____, y[0]
+                              |   +------------------------+
+            ..., x[1], x[0] --+
+                              |   +------------------------+
+                              +-->| h[1], h[3], h[5], 0    |--> ..., y[1], ____
+                                  +------------------------+
+
+            Input Hold                                          Output Commutator by 3
+                                                                (top-to-bottom)
+
+            x[n] = Input signal with sample rate fs
+            y[n] = Output signal with sample rate fs * P / Q
+            h[n] = Prototype FIR filter
+
+        The polyphase feedforward taps $h_{i, j}$ are related to the prototype feedforward taps $h_i$ by
+
+        $$h_{i, j} = h_{i + j P} .$$
+
+        If the interpolation rate $P$ is 1, then the polyphase rational resampling filter is equivalent to the
+        polyphase decimating filter. See :class:`~sdr.Decimator`.
+
+    Examples:
+        Create an input signal to resample.
+
+        .. ipython:: python
+
+            x = np.cos(np.pi / 4 * np.arange(40))
+
+        Create a polyphase filter that resamples by 7/3 using the Kaiser window method.
+
+        .. ipython:: python
+
+            fir = sdr.Resampler(7, 3); fir
+            y = fir(x)
+
+            @savefig sdr_Resampler_1.png
+            plt.figure(figsize=(8, 4)); \
+            sdr.plot.time_domain(x, marker="o", label="Input"); \
+            sdr.plot.time_domain(y, sample_rate=fir.rate, marker=".", label="Resampled"); \
+            plt.title("Resampling by 7/3 with the Kaiser window method"); \
+            plt.tight_layout();
+
+        Create a streaming polyphase filter that resamples by 7/3 using the Kaiser window method. This filter
+        preserves state between calls.
+
+        .. ipython:: python
+
+            fir = sdr.Resampler(7, 3, streaming=True); fir
+
+            y1 = fir(x[0:10]); \
+            y2 = fir(x[10:20]); \
+            y3 = fir(x[20:30]); \
+            y4 = fir(x[30:40]); \
+            y5 = fir.flush()
+
+            @savefig sdr_Resampler_2.png
+            plt.figure(figsize=(8, 4)); \
+            sdr.plot.time_domain(x, marker="o", label="Input"); \
+            sdr.plot.time_domain(y1, sample_rate=fir.rate, offset=-fir.delay/fir.rate + 0, marker=".", label="Resampled $y_1[n]$"); \
+            sdr.plot.time_domain(y2, sample_rate=fir.rate, offset=-fir.delay/fir.rate + 10, marker=".", label="Resampled $y_2[n]$"); \
+            sdr.plot.time_domain(y3, sample_rate=fir.rate, offset=-fir.delay/fir.rate + 20, marker=".", label="Resampled $y_3[n]$"); \
+            sdr.plot.time_domain(y4, sample_rate=fir.rate, offset=-fir.delay/fir.rate + 30, marker=".", label="Resampled $y_4[n]$"); \
+            sdr.plot.time_domain(y5, sample_rate=fir.rate, offset=-fir.delay/fir.rate + 40, marker=".", label="Resampled $y_5[n]$"); \
+            plt.title("Streaming resampling by 7/3 with the Kaiser window method"); \
+            plt.tight_layout();
+
+        Create a polyphase filter that resamples by 5/7 using linear method.
+
+        .. ipython:: python
+
+            fir = sdr.Resampler(5, 7); fir
+            y = fir(x)
+
+            @savefig sdr_Resampler_3.png
+            plt.figure(figsize=(8, 4)); \
+            sdr.plot.time_domain(x, marker=".", label="Input"); \
+            sdr.plot.time_domain(y, sample_rate=fir.rate, marker="o", label="Resampled"); \
+            plt.title("Resampling by 5/7 with the Kaiser window method"); \
+            plt.tight_layout();
+
+    Group:
+        dsp-multirate-filtering
+    """
+
+    def __init__(
+        self,
+        up: int,
+        down: int,
+        taps: Literal["kaiser", "linear", "linear-matlab", "zoh"] | npt.ArrayLike = "kaiser",
+        streaming: bool = False,
+    ):
+        r"""
+        Creates a polyphase FIR rational resampling filter.
+
+        Arguments:
+            up: The interpolation rate $P$.
+            down: The decimation rate $Q$.
+            taps: The multirate filter design specification.
+
+                - `"kaiser"`: The multirate filter is designed using :func:`~sdr.multirate_taps()`
+                  with arguments `up` and `down`.
+                - `"linear"`: The multirate filter is designed to linearly interpolate between samples.
+                  The filter coefficients are a length-$2P$ linear ramp $\frac{1}{P} [0, ..., P-1, P, P-1, ..., 1]$.
+                  The first output sample aligns with the first input sample.
+                - `"linear-matlab"`: The multirate filter is designed to linearly interpolate between samples.
+                  The filter coefficients are a length-$2P$ linear ramp $\frac{1}{P} [1, ..., P-1, P, P-1, ..., 0]$.
+                  This is method MATLAB uses. The first output sample is advanced from the first input sample.
+                - `"zoh"`: The multirate filter is designed to be a zero-order hold.
+                  The filter coefficients are a length-$P$ array of ones.
+                - `npt.ArrayLike`: The multirate filter feedforward coefficients $h_i$.
+
+            streaming: Indicates whether to use streaming mode. In streaming mode, previous inputs are
+                preserved between calls to :meth:`~Resampler.__call__()`.
+        """
+        if not isinstance(up, int):
+            raise TypeError(f"Argument 'up' must be an integer, not {type(up)}.")
+        if not up >= 1:
+            raise ValueError(f"Argument 'up' must be at least 1, not {up}.")
+
+        if not isinstance(down, int):
+            raise TypeError(f"Argument 'down' must be an integer, not {type(down)}.")
+        if not down >= 1:
+            raise ValueError(f"Argument 'down' must be at least 1, not {down}.")
+
+        self._up = up
+        self._down = down
+        self._rate = up / down
+        self._method: Literal["kaiser", "linear", "linear-matlab", "zoh", "custom"]
+
+        if not isinstance(taps, str):
+            self._method = "custom"
+            taps = np.asarray(taps)
+        elif taps == "kaiser":
+            self._method = "kaiser"
+            taps = multirate_taps(up, down)
+        elif taps == "linear":
+            if not up > 1:
+                raise ValueError(f"Argument 'up' must be greater than 1 to use the 'linear' method, not {up}.")
+            self._method = "linear"
+            taps = _multirate_taps_linear(up)
+        elif taps == "linear-matlab":
+            if not up > 1:
+                raise ValueError(f"Argument 'up' must be greater than 1 to use the 'linear-matlab' method, not {up}.")
+            self._method = "linear-matlab"
+            taps = _multirate_taps_linear_matlab(up)
+        elif taps == "zoh":
+            if not up > 1:
+                raise ValueError(f"Argument 'up' must be greater than 1 to use the 'zoh' method, not {up}.")
+            self._method = "zoh"
+            taps = _multirate_taps_zoh(up)
+        else:
+            raise ValueError(
+                f"Argument 'taps' must be 'kaiser', 'linear', 'linear-matlab', 'zoh', or an array-like, not {taps}."
+            )
+
+        self._polyphase_taps = polyphase_matrix(up, down, taps)
+
+        super().__init__(taps, streaming=streaming)
+
+    ##############################################################################
+    # Special methods
+    ##############################################################################
+
+    def __call__(self, x: npt.ArrayLike, mode: Literal["rate", "full"] = "rate") -> np.ndarray:
+        r"""
+        Resamples and filters the input signal $x[n]$ with the polyphase FIR filter.
+        """
+        x = np.atleast_1d(x)
+        if not x.ndim == 1:
+            raise ValueError(f"Argument 'x' must be a 1-D, not {x.ndim}-D.")
+
+        if self.up == 1:
+            y, self._state = _polyphase_decimate(x, self._state, self.polyphase_taps, mode, self.streaming)
+        else:
+            y, self._state = _polyphase_interpolate(x, self._state, self.polyphase_taps, mode, self.streaming)
+            y = y[:: self.down]  # Downsample the interpolated output
+
+        return y
+
+    def __repr__(self) -> str:
+        """
+        Returns a code-styled string representation of the object.
+
+        Examples:
+            .. ipython:: python
+
+                fir = sdr.Resampler(4, 3)
+                fir
+        """
+        if self.method == "custom":
+            h_str = np.array2string(self.taps, max_line_width=int(1e6), separator=", ", suppress_small=True)
+        else:
+            h_str = repr(self.method)
+        return f"sdr.{type(self).__name__}({self.up}, {self.down}, {h_str}, streaming={self.streaming})"
+
+    def __str__(self) -> str:
+        """
+        Returns a human-readable string representation of the object.
+
+        Examples:
+            .. ipython:: python
+
+                fir = sdr.Resampler(4, 3)
+                print(fir)
+        """
+        string = f"sdr.{type(self).__name__}:"
+        string += f"\n  order: {self.order}"
+        string += f"\n  rate: {self.up} / {self.down}"
+        string += f"\n  method: {self._method!r}"
+        string += f"\n  polyphase_taps: {self.polyphase_taps.shape} shape"
+        string += f"\n  delay: {self.delay}"
+        string += f"\n  streaming: {self.streaming}"
+        return string
+
+    ##############################################################################
+    # Streaming mode
+    ##############################################################################
+
+    def reset(self):
+        self._state = np.zeros(self.polyphase_taps.shape[1] - 1)
+
+    ##############################################################################
+    # Properties
+    ##############################################################################
+
+    @property
+    def up(self) -> int:
+        """
+        The interpolation rate $P$.
+        """
+        return self._up
+
+    @property
+    def down(self) -> int:
+        """
+        The decimation rate $Q$.
+        """
+        return self._down
+
+    @property
+    def rate(self) -> float:
+        """
+        The resampling rate $P/Q$.
+        """
+        return self._rate
+
+    @property
+    def method(self) -> Literal["kaiser", "linear", "linear-matlab", "zoh", "custom"]:
+        """
+        The method used to design the multirate filter.
+        """
+        return self._method
+
+    @property
+    def taps(self) -> npt.NDArray:
+        """
+        The prototype feedforward taps $h_i$.
+
+        Notes:
+            The prototype feedforward taps $h_i$ are the feedforward taps of the FIR filter before
+            polyphase decomposition. The polyphase feedforward taps $h_{i, j}$ are the feedforward taps
+            of the FIR filter after polyphase decomposition.
+
+            The polyphase feedforward taps $h_{i, j}$ are related to the prototype feedforward taps $h_i$ by
+
+            $$h_{i, j} = h_{i + j P} .$$
+
+        Examples:
+            .. ipython:: python
+
+                fir = sdr.Resampler(4, 3, np.arange(10))
+                fir.taps
+                fir.polyphase_taps
+        """
+        return self._taps
+
+    @property
+    def polyphase_taps(self) -> npt.NDArray:
+        """
+        The polyphase feedforward taps $h_{i, j}$.
+
+        Notes:
+            The prototype feedforward taps $h_i$ are the feedforward taps of the FIR filter before
+            polyphase decomposition. The polyphase feedforward taps $h_{i, j}$ are the feedforward taps
+            of the FIR filter after polyphase decomposition.
+
+            The polyphase feedforward taps $h_{i, j}$ are related to the prototype feedforward taps $h_i$ by
+
+            $$h_{i, j} = h_{i + j P} .$$
+
+        Examples:
+            .. ipython:: python
+
+                fir = sdr.Resampler(4, 3, np.arange(10))
+                fir.taps
+                fir.polyphase_taps
+        """
+        return self._polyphase_taps
+
+    @property
+    def delay(self) -> float:
+        return super().delay / self.down
