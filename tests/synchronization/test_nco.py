7847179d31f4956bffd2dd77e87fbbc076835521
mhostetter <matthostetter@gmail.com>
2023-11-12 09:06:24 -0500

    Add unit tests for `NCO` #133

--- tests/synchronization/test_nco.py
+++ tests/synchronization/test_nco.py
@@ -1,6 +1,28 @@
 import numpy as np
+import pytest
 
 import sdr
+
+
+def test_call_no_args():
+    nco = sdr.NCO()
+    with pytest.raises(ValueError):
+        nco()
+
+
+def test_initial_phase_is_offset():
+    K0 = 1
+    increment = 2 * np.pi / 10
+    offset = np.pi / 2
+    nco = sdr.NCO(K0, increment, offset)
+
+    assert nco.K0 == K0
+    assert nco.increment == increment
+    assert nco.offset == offset
+
+    y = nco.step(1)
+    y_truth = np.exp(1j * offset)
+    assert np.allclose(y, y_truth)
 
 
 def test_constant_increment():
@@ -15,7 +37,7 @@ def test_constant_increment():
 
     N = 100
     y = nco.step(N)
-    y_truth = np.exp(1j * (np.arange(1, N + 1) * increment + offset))
+    y_truth = np.exp(1j * (np.arange(N) * increment + offset))
     assert np.allclose(y, y_truth)
 
 
@@ -31,5 +53,16 @@ def test_constant_increment_constant_offset():
 
     N = 100
     y = nco.step(N)
-    y_truth = np.exp(1j * (np.arange(1, N + 1) * increment + offset))
+    y_truth = np.exp(1j * (np.arange(N) * increment + offset))
+    assert np.allclose(y, y_truth)
+
+
+def test_varying_phase():
+    nco = sdr.NCO()
+
+    phase = np.array([0, 1, 0, 1, 0, 1]) * np.pi
+    phase = np.repeat(phase, 5)
+
+    y = nco(phase=phase)
+    y_truth = np.exp(1j * phase)
     assert np.allclose(y, y_truth)
