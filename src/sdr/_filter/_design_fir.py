57c64500689251aec12258c7214ffc5fabe5a852
mhostetter <matthostetter@gmail.com>
2023-11-18 10:38:51 -0500

    Add `sdr.design_bandpass_fir()`
    Closes Add `sdr.design_bandpass_fir()` #148

--- src/sdr/_filter/_design_fir.py
+++ src/sdr/_filter/_design_fir.py
@@ -24,6 +24,13 @@ def _normalize(h: npt.NDArray[np.float_], norm: Literal["power", "energy", "pass
     return h
 
 
+def _normalize_passband(h: npt.NDArray[np.float_], center_freq: float) -> npt.NDArray[np.float_]:
+    order = h.size - 1
+    lo = np.exp(-1j * np.pi * center_freq * np.arange(-order // 2, order // 2 + 1))
+    h = h / np.abs(np.sum(h * lo))
+    return h
+
+
 def _ideal_lowpass(order: int, cutoff_freq: float) -> npt.NDArray[np.float_]:
     """
     Returns the ideal lowpass filter impulse response.
@@ -153,5 +160,104 @@ def design_lowpass_fir(
     h_window = _window(order, window)
     h = h_ideal * h_window
     h = _normalize(h, "passband")
+
+    return h
+
+
+@export
+def design_bandpass_fir(
+    order: int,
+    center_freq: float,
+    bandwidth: float,
+    window: None
+    | Literal["hamming", "hann", "blackman", "blackman-harris", "chebyshev", "kaiser"]
+    | npt.ArrayLike = "hamming",
+) -> npt.NDArray[np.float_]:
+    r"""
+    Designs a bandpass FIR filter impulse response $h[n]$ using the window method.
+
+    Arguments:
+        order: The filter order $N$. Must be even.
+        center_freq: The center frequency $f_{center}$, normalized to the Nyquist frequency $f_s / 2$.
+        bandwidth: The two-sided bandwidth about $f_{center}$, normalized to the Nyquist frequency $f_s / 2$.
+        window: The time-domain window to use.
+
+            - `None`: No windowing. Equivalently, a length-$N + 1$ vector of ones.
+            - `"hamming"`: Hamming window, see :func:`scipy.signal.windows.hamming`.
+            - `"hann"`: Hann window, see :func:`scipy.signal.windows.hann`.
+            - `"blackman"`: Blackman window, see :func:`scipy.signal.windows.blackman`.
+            - `"blackman-harris"`: Blackman-Harris window, see :func:`scipy.signal.windows.blackmanharris`.
+            - `"chebyshev"`: Chebyshev window, see :func:`scipy.signal.windows.chebwin`. The sidelobe attenuation
+              is 60 dB.
+            - `"kaiser"`: Kaiser window, see :func:`scipy.signal.windows.kaiser`. The beta parameter is 0.5.
+            - `npt.ArrayLike`: A custom window. Must be a length-$N + 1$ vector.
+
+    Returns:
+        The filter impulse response $h[n]$ with length $N + 1$.
+
+    References:
+        - https://www.mathworks.com/help/dsp/ref/designbandpassfir.html
+
+    Examples:
+        Design a length-101 bandpass FIR filter with center frequency $f_{center} = 0.4 \cdot f_s / 2$
+        and bandwidth $0.1 \cdot f_s / 2$, using a Hamming window.
+
+        .. ipython:: python
+
+            h_hamming = sdr.design_bandpass_fir(100, 0.4, 0.1, window="hamming")
+
+            @savefig sdr_design_bandpass_fir_1.png
+            plt.figure(figsize=(8, 4)); \
+            sdr.plot.impulse_response(h_hamming);
+
+            @savefig sdr_design_bandpass_fir_2.png
+            plt.figure(figsize=(8, 4)); \
+            sdr.plot.magnitude_response(h_hamming);
+
+        Compare filter designs using different windows.
+
+        .. ipython:: python
+
+            h_hann = sdr.design_bandpass_fir(100, 0.4, 0.1, window="hann"); \
+            h_blackman = sdr.design_bandpass_fir(100, 0.4, 0.1, window="blackman"); \
+            h_blackman_harris = sdr.design_bandpass_fir(100, 0.4, 0.1, window="blackman-harris"); \
+            h_chebyshev = sdr.design_bandpass_fir(100, 0.4, 0.1, window="chebyshev"); \
+            h_kaiser = sdr.design_bandpass_fir(100, 0.4, 0.1, window="kaiser")
+
+            @savefig sdr_design_bandpass_fir_3.png
+            plt.figure(figsize=(8, 4)); \
+            sdr.plot.magnitude_response(h_hamming, label="Hamming"); \
+            sdr.plot.magnitude_response(h_hann, label="Hann"); \
+            sdr.plot.magnitude_response(h_blackman, label="Blackman"); \
+            sdr.plot.magnitude_response(h_blackman_harris, label="Blackman-Harris"); \
+            sdr.plot.magnitude_response(h_chebyshev, label="Chebyshev"); \
+            sdr.plot.magnitude_response(h_kaiser, label="Kaiser"); \
+            plt.legend(); \
+            plt.ylim(-100, 10);
+
+    Group:
+        dsp-fir-filtering
+    """
+    if not isinstance(order, int):
+        raise TypeError(f"Argument 'order' must be an integer, not {type(order).__name__}.")
+    if not order % 2 == 0:
+        raise ValueError(f"Argument 'order' must be even, not {order}.")
+
+    if not isinstance(center_freq, (int, float)):
+        raise TypeError(f"Argument 'center_freq' must be a number, not {type(center_freq).__name__}.")
+    if not 0 <= center_freq <= 1:
+        raise ValueError(f"Argument 'center_freq' must be between 0 and 0.5, not {center_freq}.")
+
+    if not isinstance(bandwidth, (int, float)):
+        raise TypeError(f"Argument 'bandwidth' must be a number, not {type(bandwidth).__name__}.")
+    if not 0 <= center_freq + bandwidth / 2 <= 1:
+        raise ValueError(f"Argument 'bandwidth' must be between 0 and 0.5 - 'center_freq', not {bandwidth}.")
+    if not 0 <= center_freq - bandwidth / 2 <= 1:
+        raise ValueError(f"Argument 'bandwidth' must be between 0 and 'center_freq', not {bandwidth}.")
+
+    h_ideal = _ideal_lowpass(order, center_freq + bandwidth / 2) - _ideal_lowpass(order, center_freq - bandwidth / 2)
+    h_window = _window(order, window)
+    h = h_ideal * h_window
+    h = _normalize_passband(h, center_freq)
 
     return h
