bb3d07f0f1e2b473bf1623fcfe2d09b52769fdd2
mhostetter <matthostetter@gmail.com>
2023-11-19 10:48:55 -0500

    Add unit tests for `design_highpass_fir()`

--- tests/dsp/fir/helper.py
+++ tests/dsp/fir/helper.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 
 import sdr
 
 
-def debug_plot(h, h_truth):
+def debug_plot(h: npt.NDArray, h_truth: npt.NDArray):
     plt.figure()
     sdr.plot.time_domain(h_truth, marker=".", label="h_truth")
     sdr.plot.time_domain(h, marker="x", label="h")
@@ -18,7 +19,7 @@ def debug_plot(h, h_truth):
     plt.show()
 
 
-def verify_impulse_response(h, h_truth):
+def verify_impulse_response(h: npt.NDArray, h_truth: npt.NDArray, atol: float = 1e-6):
     # debug_plot(h, h_truth)
 
     # MATLAB sets the center of the passband to 0 dB gain. We set the average passband gain to 0 dB.
@@ -26,4 +27,4 @@ def verify_impulse_response(h, h_truth):
     h *= sdr.rms_voltage(h_truth) / sdr.rms_voltage(h)
 
     assert h.shape == h_truth.shape
-    assert np.allclose(h, h_truth)
+    assert np.allclose(h, h_truth, atol=atol)
