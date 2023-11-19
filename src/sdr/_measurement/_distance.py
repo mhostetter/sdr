f1803b7ceaa8a9339c27d036f1a06616d3f3176c
mhostetter <matthostetter@gmail.com>
2023-11-19 12:05:49 -0500

    Add `sdr.hamming()`
    Closes #63

--- src/sdr/_measurement/_distance.py
+++ src/sdr/_measurement/_distance.py
@@ -36,3 +36,32 @@ def euclidean(
     y = np.asarray(y)
     d = np.sqrt(np.sum(np.abs(x - y) ** 2, axis=axis))
     return d
+
+
+@export
+def hamming(
+    x: npt.NDArray[np.int_],
+    y: npt.NDArray[np.int_],
+    axis: int | tuple[int, ...] | None = None,
+) -> npt.NDArray[np.int_]:
+    r"""
+    Measures the Hamming distance between two signals $x[n]$ and $y[n]$.
+
+    $$d = \sum_{n=0}^{N-1} x[n] ^ y[n]$$
+
+    Arguments:
+        x: The time-domain signal $x[n]$.
+        y: The time-domain signal $y[n]$.
+        axis: Axis or axes along which to compute the distance. The default is `None`, which computes the distance
+            across the entire array.
+
+    Returns:
+        The Hamming distance between $x[n]$ and $y[n]$.
+
+    Group:
+        measurement-distance
+    """
+    x = np.asarray(x)
+    y = np.asarray(y)
+    d = np.sum(np.bitwise_xor(x, y), axis=axis)
+    return d
