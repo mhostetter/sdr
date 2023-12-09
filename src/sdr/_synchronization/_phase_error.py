f0cad0906c3f0a79538061de0d153504c4224614
mhostetter <matthostetter@gmail.com>
2023-12-09 13:33:46 -0500

    Add `sdr.MLPED`
    Fixes #198

--- src/sdr/_synchronization/_phase_error.py
+++ src/sdr/_synchronization/_phase_error.py
@@ -34,8 +34,11 @@ class PED:
 
         .. ipython:: python
 
-            qpsk = sdr.PSK(4); \
-            ped = sdr.PED(); \
+            qpsk = sdr.PSK(4)
+            ped = sdr.PED()
+
+        .. ipython:: python
+
             error, da_error = ped.data_aided_error(qpsk); \
             error, dd_error = ped.decision_directed_error(qpsk)
 
@@ -123,8 +126,122 @@ class PED:
         return 1.0
 
 
+@export
+class MLPED(PED):
+    r"""
+    Implements a maximum-likelihood phase error detector (ML-PED).
+
+    Notes:
+        The data-aided ML-PED is computed using the received complex symbols $\tilde{a}[k]$ and the known
+        transmitted complex symbols $a[k]$.
+
+        $$\theta_{e,DA}[k] = \Im(\tilde{a}[k]) \cdot \Re(a[k]) - \Re(\tilde{a}[k]) \cdot \Im(a[k])) $$
+
+        The data-aided ML-PED is computed using the received complex symbols $\tilde{a}[k]$ and the complex
+        symbol decisions $\hat{a}[k]$.
+
+        $$\theta_{e,DA}[k] = \Im(\tilde{a}[k]) \cdot \Re(\hat{a}[k]) - \Re(\tilde{a}[k]) \cdot \Im(\hat{a}[k]) $$
+
+    References:
+        - Michael Rice, *Digital Communications: A Discrete-Time Approach*, Section 7.2.2.
+
+    Examples:
+        Compare the data-aided and decision-directed ML-PEDs on QPSK modulation.
+
+        .. ipython:: python
+
+            qpsk = sdr.PSK(4)
+            A_rx, A_ref = 5, 3
+            ped = sdr.MLPED(A_rx, A_ref)
+
+        .. ipython:: python
+
+            error, da_error = ped.data_aided_error(qpsk); \
+            error, dd_error = ped.decision_directed_error(qpsk)
+
+            @savefig sdr_MLPED_1.png
+            plt.figure(figsize=(8, 4)); \
+            plt.plot(error, da_error, label="Data-aided"); \
+            plt.plot(error, dd_error, label="Decision-directed"); \
+            plt.grid(True); \
+            plt.legend(); \
+            plt.xlabel("Phase of received symbols (radians)"); \
+            plt.ylabel("Phase error (radians)"); \
+            plt.title("Comparison of data-aided and decision-directed ML-PEDs on QPSK"); \
+            plt.tight_layout();
+
+        Observe that the slope of the phase error $K_p = A_{rx,rms} A_{ref,rms}$ is the same for both the data-aided
+        and decision-directed PEDs. It's very important to observe that the gain of the ML-PED is scaled by the
+        received signal amplitude $A_{rx,rms}$ and the reference signal amplitude $A_{ref,rms}$. Because of this,
+        the ML-PED should only be used with automatic gain control (AGC).
+
+        .. ipython:: python
+
+            ped.gain
+            A_rx * A_ref
+
+        Also note that the unambiguous range of the data-aided ML-PED is $[-\pi, \pi)$ and the
+        decision-directed ML-PED is $[-\pi/M, \pi/M)$.
+
+    Group:
+        synchronization-ped
+    """
+
+    def __init__(self, A_received: float = 1, A_reference: float = 1) -> None:
+        """
+        Initializes the ML-PED.
+
+        Arguments:
+            A_received: The received signal RMS amplitude $A_{rx,rms}$.
+            A_reference: The reference signal RMS amplitude $A_{ref,rms}$.
+        """
+        self._A_received = A_received
+        self._A_reference = A_reference
+
+    def __call__(self, received: npt.ArrayLike, reference: npt.ArrayLike) -> np.ndarray:
+        received = np.asarray(received)
+        reference = np.asarray(reference)
+        return received.imag * reference.real - received.real * reference.imag
+
+    def data_aided_error(
+        self, modem: LinearModulation, n_points: int = 1000
+    ) -> tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]]:
+        return _data_aided_error(self, modem, n_points, self._A_received, self._A_reference)
+
+    def decision_directed_error(
+        self, modem: LinearModulation, n_points: int = 1000
+    ) -> tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]]:
+        return _decision_directed_error(self, modem, n_points, self._A_received, self._A_reference)
+
+    @property
+    def gain(self) -> float:
+        return self.A_received * self.A_reference
+
+    @property
+    def A_received(self) -> float:
+        """
+        (Settable) The received signal RMS amplitude $A_{rx,rms}$.
+        """
+        return self._A_received
+
+    @A_received.setter
+    def A_received(self, A_received: float) -> None:
+        self._A_received = A_received
+
+    @property
+    def A_reference(self) -> float:
+        """
+        (Settable) The reference signal RMS amplitude $A_{ref,rms}$.
+        """
+        return self._A_reference
+
+    @A_reference.setter
+    def A_reference(self, A_reference: float) -> None:
+        self._A_reference = A_reference
+
+
 def _data_aided_error(
-    ped: PED, modem: LinearModulation, n_points: int = 1000
+    ped: PED, modem: LinearModulation, n_points: int = 1000, A_received: float = 1, A_reference: float = 1
 ) -> tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]]:
     if not isinstance(ped, PED):
         raise TypeError(f"Argument 'ped' must be a PED, not {type(ped)}.")
@@ -137,14 +254,14 @@ def _data_aided_error(
     da_error = np.zeros(n_points, dtype=float)
     for reference in modem.symbol_map:
         received = reference * np.exp(1j * error)
-        da_error += ped(received, reference)
+        da_error += ped(A_received * received, A_reference * reference)
     da_error /= modem.order
 
     return error, da_error
 
 
 def _decision_directed_error(
-    ped: PED, modem: LinearModulation, n_points: int = 1000
+    ped: PED, modem: LinearModulation, n_points: int = 1000, A_received: float = 1, A_reference: float = 1
 ) -> tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]]:
     if not isinstance(ped, PED):
         raise TypeError(f"Argument 'ped' must be a PED, not {type(ped)}.")
@@ -158,7 +275,7 @@ def _decision_directed_error(
     for reference in modem.symbol_map:
         received = reference * np.exp(1j * error)
         _, decided = modem.decide_symbols(received)
-        dd_error += ped(received, decided)
+        dd_error += ped(A_received * received, A_reference * decided)
     dd_error /= modem.order
 
     return error, dd_error
