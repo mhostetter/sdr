a83a274f4e3f30f597834929fcd825cf77a6f17c
mhostetter <matthostetter@gmail.com>
2023-12-14 17:26:26 -0500

    Improve differentiator bandwidth
    Fixes #207

--- tests/dsp/fir/test_differentiator.py
+++ tests/dsp/fir/test_differentiator.py
@@ -1,25 +1,75 @@
 import numpy as np
+import pytest
 
 import sdr
 
 
-def test_gaussian():
-    fir = sdr.Differentiator()
-    x = sdr.gaussian(0.3, 5, 10)
-    y = fir(x)
+def test_exception():
+    with pytest.raises(TypeError):
+        sdr.Differentiator(4.0)
+    with pytest.raises(ValueError):
+        sdr.Differentiator(0)
+    with pytest.raises(ValueError):
+        sdr.Differentiator(3)
 
-    xx = np.concatenate(([0], x, [0]))
-    y_truth = np.diff(xx)
-
-    assert np.allclose(y, y_truth)
+    with pytest.raises(ValueError):
+        sdr.Differentiator(10, window="bad_window")
 
 
-def test_raised_cosine():
-    fir = sdr.Differentiator()
-    x = sdr.root_raised_cosine(0.1, 8, 10)
-    y = fir(x)
+def test_10_hamming():
+    fir = sdr.Differentiator(10, window="hamming")
+    h = fir.taps
+    h_truth = np.array(
+        [
+            0.016,
+            -0.04196305,
+            0.13261739,
+            -0.34107391,
+            0.91214782,
+            0.0,
+            -0.91214782,
+            0.34107391,
+            -0.13261739,
+            0.04196305,
+            -0.016,
+        ]
+    )
+    assert np.allclose(h, h_truth)
 
-    xx = np.concatenate(([0], x, [0]))
-    y_truth = np.diff(xx)
 
-    assert np.allclose(y, y_truth)
+def test_20_kaiser():
+    fir = sdr.Differentiator(20, window=("kaiser", 0.5))
+    h = fir.taps
+    h_truth = np.array(
+        [
+            -0.09403062,
+            0.10572284,
+            -0.1201978,
+            0.13864545,
+            -0.16304937,
+            0.19698045,
+            -0.247581,
+            0.33151712,
+            -0.49878825,
+            0.99939384,
+            0.0,
+            -0.99939384,
+            0.49878825,
+            -0.33151712,
+            0.247581,
+            -0.19698045,
+            0.16304937,
+            -0.13864545,
+            0.1201978,
+            -0.10572284,
+            0.09403062,
+        ]
+    )
+    assert np.allclose(h, h_truth)
+
+
+def test_central_difference():
+    fir = sdr.Differentiator(2, window=None)
+    h = fir.taps
+    h_truth = np.array([1, 0, -1])
+    assert np.allclose(h, h_truth)
