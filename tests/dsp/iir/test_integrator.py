a9a3543a999a1a822db6df23acfc2b1c0b1abdb6
mhostetter <matthostetter@gmail.com>
2023-12-14 18:36:16 -0500

    Implement backward, forward, and trapezoidal IIR integration
    Fixes #209

--- tests/dsp/iir/test_integrator.py
+++ tests/dsp/iir/test_integrator.py
@@ -1,23 +1,49 @@
 import numpy as np
+import pytest
 
 import sdr
 
 
+def test_exceptions():
+    with pytest.raises(ValueError):
+        sdr.Integrator("invalid")
+
+
+def test_backward():
+    iir = sdr.Integrator("backward")
+    b_truth = [0, 1]
+    a_truth = [1, -1]
+    assert np.allclose(iir.b_taps, b_truth)
+    assert np.allclose(iir.a_taps, a_truth)
+
+
+def test_trapezoidal():
+    iir = sdr.Integrator("trapezoidal")
+    b_truth = [0.5, 0.5]
+    a_truth = [1, -1]
+    assert np.allclose(iir.b_taps, b_truth)
+    assert np.allclose(iir.a_taps, a_truth)
+
+
+def test_forward():
+    iir = sdr.Integrator("forward")
+    b_truth = [1]
+    a_truth = [1, -1]
+    assert np.allclose(iir.b_taps, b_truth)
+    assert np.allclose(iir.a_taps, a_truth)
+
+
 def test_gaussian():
-    fir = sdr.Integrator()
+    iir = sdr.Integrator("forward")
     x = sdr.gaussian(0.3, 5, 10)
-    y = fir(x)
-
+    y = iir(x)
     y_truth = np.cumsum(x)
-
     assert np.allclose(y, y_truth)
 
 
 def test_raised_cosine():
-    fir = sdr.Integrator()
+    iir = sdr.Integrator("forward")
     x = sdr.root_raised_cosine(0.1, 8, 10)
-    y = fir(x)
-
+    y = iir(x)
     y_truth = np.cumsum(x)
-
     assert np.allclose(y, y_truth)
