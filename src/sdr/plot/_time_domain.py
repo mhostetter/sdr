2ea43e8dd079342cea5392f12d001bd267f62654
mhostetter <matthostetter@gmail.com>
2024-01-21 14:01:29 -0500

    Enable rcParams in a more consistent way

--- src/sdr/plot/_time_domain.py
+++ src/sdr/plot/_time_domain.py
@@ -109,40 +109,39 @@ def time_domain(
     Group:
         plot-time-domain
     """
-    if len(args) == 1:
-        x = args[0]
-        t = None
-    elif len(args) == 2:
-        t, x = args
-    else:
-        raise ValueError(f"Expected 1 or 2 positional arguments, got {len(args)}.")
-
-    if not x.ndim == 1:
-        raise ValueError(f"Argument 'x' must be 1-D, not {x.ndim}-D.")
-
-    if sample_rate is None:
-        sample_rate_provided = False
-        sample_rate = 1
-    else:
-        sample_rate_provided = True
-        if not isinstance(sample_rate, (int, float)):
-            raise TypeError(f"Argument 'sample_rate' must be a number, not {type(sample_rate)}.")
-
-    if t is None:
-        if centered:
-            if x.size % 2 == 0:
-                t = np.arange(-x.size // 2, x.size // 2) / sample_rate
-            else:
-                t = np.arange(-(x.size - 1) // 2, (x.size + 1) // 2) / sample_rate
-        else:
-            t = np.arange(x.size) / sample_rate + offset
-
-    if sample_rate_provided:
-        units, scalar = time_units(t)
-        t *= scalar
-
-    # with plt.style.context(Path(__file__).parent / ".." / "presentation.mplstyle"):
     with plt.rc_context(RC_PARAMS):
+        if len(args) == 1:
+            x = args[0]
+            t = None
+        elif len(args) == 2:
+            t, x = args
+        else:
+            raise ValueError(f"Expected 1 or 2 positional arguments, got {len(args)}.")
+
+        if not x.ndim == 1:
+            raise ValueError(f"Argument 'x' must be 1-D, not {x.ndim}-D.")
+
+        if sample_rate is None:
+            sample_rate_provided = False
+            sample_rate = 1
+        else:
+            sample_rate_provided = True
+            if not isinstance(sample_rate, (int, float)):
+                raise TypeError(f"Argument 'sample_rate' must be a number, not {type(sample_rate)}.")
+
+        if t is None:
+            if centered:
+                if x.size % 2 == 0:
+                    t = np.arange(-x.size // 2, x.size // 2) / sample_rate
+                else:
+                    t = np.arange(-(x.size - 1) // 2, (x.size + 1) // 2) / sample_rate
+            else:
+                t = np.arange(x.size) / sample_rate + offset
+
+        if sample_rate_provided:
+            units, scalar = time_units(t)
+            t *= scalar
+
         real_or_complex_plot(t, x, diff=diff, **kwargs)
         if sample_rate_provided:
             plt.xlabel(f"Time ({units})")
@@ -199,68 +198,68 @@ def raster(
     Group:
         plot-time-domain
     """
-    if not x.ndim in [1, 2]:
-        raise ValueError(f"Argument 'x' must be 1-D or 2-D, not {x.ndim}-D.")
-    if np.iscomplexobj(x):
-        raise ValueError("Argument 'x' must be real, not complex.")
+    with plt.rc_context(RC_PARAMS):
+        if not x.ndim in [1, 2]:
+            raise ValueError(f"Argument 'x' must be 1-D or 2-D, not {x.ndim}-D.")
+        if np.iscomplexobj(x):
+            raise ValueError("Argument 'x' must be real, not complex.")
 
-    if x.ndim == 1:
-        if not length is not None:
-            raise ValueError("Argument 'length' must be specified if 'x' is 1-D.")
-        if not isinstance(length, int):
-            raise TypeError(f"Argument 'length' must be an integer, not {type(length)}.")
-        if not 1 <= length <= x.size:
-            raise ValueError(f"Argument 'length' must be at least 1 and less than the length of 'x', not {length}.")
+        if x.ndim == 1:
+            if not length is not None:
+                raise ValueError("Argument 'length' must be specified if 'x' is 1-D.")
+            if not isinstance(length, int):
+                raise TypeError(f"Argument 'length' must be an integer, not {type(length)}.")
+            if not 1 <= length <= x.size:
+                raise ValueError(f"Argument 'length' must be at least 1 and less than the length of 'x', not {length}.")
 
-        if stride is None:
-            stride = length
-        elif not isinstance(stride, int):
-            raise TypeError(f"Argument 'stride' must be an integer, not {type(stride)}.")
-        elif not 1 <= stride <= x.size:
-            raise ValueError(f"Argument 'stride' must be at least 1 and less than the length of 'x', not {stride}.")
+            if stride is None:
+                stride = length
+            elif not isinstance(stride, int):
+                raise TypeError(f"Argument 'stride' must be an integer, not {type(stride)}.")
+            elif not 1 <= stride <= x.size:
+                raise ValueError(f"Argument 'stride' must be at least 1 and less than the length of 'x', not {stride}.")
 
-        # Compute the strided data and format into segments for LineCollection
-        N_rasters = (x.size - length) // stride + 1
-        x_strided = np.lib.stride_tricks.as_strided(
-            x, shape=(N_rasters, length), strides=(x.strides[0] * stride, x.strides[0]), writeable=False
-        )
-    else:
-        if not length is None:
-            raise ValueError("Argument 'length' can not be specified if 'x' is 2-D.")
-        if not stride is None:
-            raise ValueError("Argument 'stride' can not be specified if 'x' is 2-D.")
+            # Compute the strided data and format into segments for LineCollection
+            N_rasters = (x.size - length) // stride + 1
+            x_strided = np.lib.stride_tricks.as_strided(
+                x, shape=(N_rasters, length), strides=(x.strides[0] * stride, x.strides[0]), writeable=False
+            )
+        else:
+            if not length is None:
+                raise ValueError("Argument 'length' can not be specified if 'x' is 2-D.")
+            if not stride is None:
+                raise ValueError("Argument 'stride' can not be specified if 'x' is 2-D.")
 
-        N_rasters, length = x.shape
-        x_strided = x
+            N_rasters, length = x.shape
+            x_strided = x
 
-    if sample_rate is None:
-        sample_rate_provided = False
-        t = np.arange(length)
-    else:
-        sample_rate_provided = True
-        if not isinstance(sample_rate, (int, float)):
-            raise TypeError(f"Argument 'sample_rate' must be a number, not {type(sample_rate)}.")
-        t = np.arange(length) / sample_rate
-        units, scalar = time_units(t)
-        t *= scalar
+        if sample_rate is None:
+            sample_rate_provided = False
+            t = np.arange(length)
+        else:
+            sample_rate_provided = True
+            if not isinstance(sample_rate, (int, float)):
+                raise TypeError(f"Argument 'sample_rate' must be a number, not {type(sample_rate)}.")
+            t = np.arange(length) / sample_rate
+            units, scalar = time_units(t)
+            t *= scalar
 
-    if persistence:
-        default_kwargs = {
-            "bins": (800, 200),
-            "cmap": "rainbow",
-            "norm": "log",
-            "rasterized": True,
-            "show_zero": False,
-        }
-        kwargs = {**default_kwargs, **kwargs}
+        if persistence:
+            default_kwargs = {
+                "bins": (800, 200),
+                "cmap": "rainbow",
+                "norm": "log",
+                "rasterized": True,
+                "show_zero": False,
+            }
+            kwargs = {**default_kwargs, **kwargs}
 
-        bins = kwargs.pop("bins")
-        t_fine = np.linspace(t.min(), t.max(), bins[0])
-        x_fine = np.concatenate([np.interp(t_fine, t, x_row) for x_row in x_strided])
-        t_fine = np.broadcast_to(t_fine, (x_strided.shape[0], bins[0])).ravel()
-        h, t_edges, x_edges = np.histogram2d(t_fine, x_fine, bins=bins)
+            bins = kwargs.pop("bins")
+            t_fine = np.linspace(t.min(), t.max(), bins[0])
+            x_fine = np.concatenate([np.interp(t_fine, t, x_row) for x_row in x_strided])
+            t_fine = np.broadcast_to(t_fine, (x_strided.shape[0], bins[0])).ravel()
+            h, t_edges, x_edges = np.histogram2d(t_fine, x_fine, bins=bins)
 
-        with plt.rc_context(RC_PARAMS):
             cmap = kwargs.pop("cmap")  # Need to pop cmap to avoid passing it twice to pcolormesh
             cmap = plt.colormaps[cmap]
             show_zero = kwargs.pop("show_zero")
@@ -272,27 +271,26 @@ def raster(
             pcm = plt.pcolormesh(t_edges, x_edges, h.T, cmap=cmap, **kwargs)
             if colorbar:
                 plt.colorbar(pcm, label="Points", pad=0.05)
-    else:
-        segments = [np.column_stack([t, x_raster]) for x_raster in x_strided]
-
-        # Set the default keyword arguments and override with user-specified keyword arguments
-        default_kwargs = {
-            "linewidths": 1,
-            "linestyles": "solid",
-            "cmap": "rainbow",
-        }
-        if color == "index":
-            default_kwargs["array"] = np.arange(N_rasters)
         else:
-            default_kwargs["colors"] = color
-        kwargs = {**default_kwargs, **kwargs}
+            segments = [np.column_stack([t, x_raster]) for x_raster in x_strided]
 
-        line_collection = LineCollection(
-            segments,
-            **kwargs,
-        )
+            # Set the default keyword arguments and override with user-specified keyword arguments
+            default_kwargs = {
+                "linewidths": 1,
+                "linestyles": "solid",
+                "cmap": "rainbow",
+            }
+            if color == "index":
+                default_kwargs["array"] = np.arange(N_rasters)
+            else:
+                default_kwargs["colors"] = color
+            kwargs = {**default_kwargs, **kwargs}
 
-        with plt.rc_context(RC_PARAMS):
+            line_collection = LineCollection(
+                segments,
+                **kwargs,
+            )
+
             ax = plt.gca()
             ax.add_collection(line_collection)
             ax.set_xlim(t.min(), t.max())
@@ -301,7 +299,6 @@ def raster(
             if colorbar and color == "index":
                 plt.colorbar(line_collection, label="Raster Index", pad=0.05)
 
-    with plt.rc_context(RC_PARAMS):
         if sample_rate_provided:
             plt.xlabel(f"Time ({units})")
         else:
