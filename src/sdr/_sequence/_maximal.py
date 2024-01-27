a118faf99df83154a1e6885825b305646eac41a0
mhostetter <matthostetter@gmail.com>
2024-01-27 16:04:24 -0500

    Add bipolar output for m-sequences

--- src/sdr/_sequence/_maximal.py
+++ src/sdr/_sequence/_maximal.py
@@ -13,6 +13,7 @@ from galois.typing import PolyLike
 from typing_extensions import Literal
 
 from .._helper import export
+from ._conversion import code_to_sequence, field_to_code
 from ._lfsr import FLFSR
 
 
@@ -33,6 +34,16 @@ def m_sequence(
     index: int = 1,
     output: Literal["field"] = "decimal",
 ) -> galois.FieldArray:
+    ...
+
+
+@overload
+def m_sequence(
+    degree: int,
+    poly: PolyLike | None = None,
+    index: int = 1,
+    output: Literal["bipolar"] = "decimal",
+) -> npt.NDArray[np.float_]:
     ...
 
 
@@ -60,6 +71,7 @@ def m_sequence(
 
             - `"decimal"` (default): The m-sequence with decimal values in $[0, q^n)$.
             - `"field"`: The m-sequence as a Galois field array over $\mathrm{GF}(q^n)$.
+            - `"bipolar"`: The m-sequence with bipolar values of 1 and -1. Only valid for $q = 2$.
 
     Returns:
         The length-$q^n - 1$ m-sequence.
@@ -113,17 +125,21 @@ def m_sequence(
     if not isinstance(index, int):
         raise TypeError(f"Argument 'index' must be an integer, not {type(index)}.")
     if not 1 <= index < q**degree:
-        raise ValueError(f"Argument 'index' must be in [1, q^n - 1], not {index}.")
+        raise ValueError(f"Argument 'index' must be in [1, q^n), not {index}.")
     state_poly = Poly.Int(index, field=poly.field)
     state_vector = state_poly.coefficients(degree)
 
-    if output not in ["decimal", "field"]:
-        raise ValueError(f"Argument 'output' must be 'decimal' or 'field', not {output}.")
+    if output not in ["decimal", "field", "bipolar"]:
+        raise ValueError(f"Argument 'output' must be 'decimal', 'field', or 'bipolar', not {output}.")
 
     lfsr = FLFSR(poly, state=state_vector)
     code = lfsr.step(q**degree - 1)
 
     if output == "decimal":
-        return code.view(np.ndarray)
-    else:
+        return field_to_code(code)
+    elif output == "field":
         return code
+    else:
+        if not q == 2:
+            raise ValueError(f"Argument 'output' can only be 'bipolar' when q = 2, not {q}.")
+        return code_to_sequence(field_to_code(code))
