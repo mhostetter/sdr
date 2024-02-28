8c36c4aaaf9ed0967983cca2be0c46899c32e017
mhostetter <matthostetter@gmail.com>
2024-02-27 22:37:57 -0500

    Make `coherent_gain_loss()` return a positive value

--- tests/link_budgets/test_coherent_gain_loss.py
+++ tests/link_budgets/test_coherent_gain_loss.py
@@ -5,11 +5,11 @@ import sdr
 
 
 def test_scalar():
-    assert sdr.coherent_gain_loss(1e-3, 235) == pytest.approx(-0.8038919141626675)
+    assert sdr.coherent_gain_loss(1e-3, 235) == pytest.approx(0.8038919141626675)
 
 
 def test_vector():
     cgl = sdr.coherent_gain_loss(1e-3, [0, 100, 200, 300, 400, 500])
-    cgl_truth = np.array([0.0, -0.14335017, -0.57922366, -1.32626966, -2.42007077, -3.92239754])
+    cgl_truth = np.array([0.0, 0.14335017, 0.57922366, 1.32626966, 2.42007077, 3.92239754])
     assert isinstance(cgl, np.ndarray)
     assert np.allclose(cgl, cgl_truth)
