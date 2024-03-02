d563436553d40dfcb5a9876ad42583b45ec3b93f
mhostetter <matthostetter@gmail.com>
2024-03-02 14:21:44 -0500

    Separate into two files

--- src/sdr/_detection/__init__.py
+++ src/sdr/_detection/__init__.py
@@ -1,7 +1,8 @@
 """
 A subpackage for various detection algorithms.
 """
+from ._coherent_integration import *
 from ._correlator import *
 from ._energy import *
-from ._integration import *
+from ._non_coherent_integration import *
 from ._theory import *
