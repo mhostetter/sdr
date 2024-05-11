29cbcc3e50bdee12f7b66f324382e29815fdc3f0
mhostetter <matthostetter@gmail.com>
2024-05-11 14:02:15 -0400

    Simplify calculation of non-coherent gain

--- tests/detection/test_non_coherent_gain.py
+++ tests/detection/test_non_coherent_gain.py
@@ -20,7 +20,7 @@ def test_scalar_snr_in():
     assert sdr.non_coherent_gain(1, 10, snr_ref="input") == pytest.approx(0.0)
     assert sdr.non_coherent_gain(2, 10, snr_ref="input") == pytest.approx(2.499445060713011)
     assert sdr.non_coherent_gain(10, 10, snr_ref="input") == pytest.approx(8.666092814306324)
-    assert sdr.non_coherent_gain(20, 10, snr_ref="input") == pytest.approx(11.457842213037324)
+    assert sdr.non_coherent_gain(20, 10, snr_ref="input") == pytest.approx(11.410342926869486)
 
 
 def test_scalar_snr_out():
@@ -34,7 +34,7 @@ def test_vector_snr_in():
     snr = 10
     n_nc = np.array([1, 2, 10, 20])
     g_nc = sdr.non_coherent_gain(n_nc, snr, snr_ref="input")
-    g_nc_truth = np.array([0.0, 2.499445060713011, 8.666092814306324, 11.457842213037324])
+    g_nc_truth = np.array([0.0, 2.499445060713011, 8.666092814306324, 11.410342926869486])
     assert isinstance(g_nc, np.ndarray)
     assert np.allclose(g_nc, g_nc_truth)
 
