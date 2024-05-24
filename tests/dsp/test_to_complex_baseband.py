4fc70591feffc4ece115788a78181e60fd243200
mhostetter <matthostetter@gmail.com>
2024-05-23 22:32:33 -0400

    Rename `to_complex_bb()` to `to_complex_baseband()`
    Fixes #352

--- tests/dsp/test_to_complex_baseband.py
+++ tests/dsp/test_to_complex_baseband.py
@@ -8,8 +8,8 @@ def test_exceptions():
     with pytest.raises(ValueError):
         # x_r must be 1D
         x_r = np.zeros((2, 2), dtype=float)
-        sdr.to_complex_bb(x_r)
+        sdr.to_complex_baseband(x_r)
     with pytest.raises(ValueError):
         # x_r must be real
         x_r = np.zeros(4, dtype=complex)
-        sdr.to_complex_bb(x_r)
+        sdr.to_complex_baseband(x_r)
