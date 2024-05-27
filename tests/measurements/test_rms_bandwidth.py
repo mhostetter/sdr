4dfa2a81da17a7f12efd9b5253a3f5c02d3d14e1
mhostetter <matthostetter@gmail.com>
2024-05-27 11:35:24 -0400

    Add unit tests for `sdr.rms_bandwidth()`

---
+++ tests/measurements/test_rms_bandwidth.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pytest
+
+import sdr
+
+
+def test_psk_rect():
+    rng = np.random.default_rng(0)
+    psk = sdr.PSK(2, pulse_shape="rect")
+    s = rng.integers(0, psk.order, 1_000)
+    x = psk.modulate(s)
+    b_rms = sdr.rms_bandwidth(x, sample_rate=psk.sps)
+    assert b_rms == pytest.approx(0.7461700620944993)
+
+
+def test_psk_srrc():
+    rng = np.random.default_rng(0)
+    psk = sdr.PSK(2, pulse_shape="srrc")
+    s = rng.integers(0, psk.order, 1_000)
+    x = psk.modulate(s)
+    b_rms = sdr.rms_bandwidth(x, sample_rate=psk.sps)
+    assert b_rms == pytest.approx(0.2900015177082325)
