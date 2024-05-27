9e1e7b5017aa58efc156d7f991091eb06cf5726e
mhostetter <matthostetter@gmail.com>
2024-05-27 16:54:10 -0400

    Loosen test tolerances

--- tests/measurements/test_rms_integration_time.py
+++ tests/measurements/test_rms_integration_time.py
@@ -10,7 +10,7 @@ def test_psk_rect():
     s = rng.integers(0, psk.order, 1_000)  # Signal is 1000 seconds long
     x = psk.modulate(s)
     t_rms = sdr.rms_integration_time(x, sample_rate=psk.sps)
-    assert t_rms == pytest.approx(288.672247843467)
+    assert t_rms == pytest.approx(288.672247843467, 1e-3)
 
 
 def test_psk_srrc():
@@ -19,7 +19,7 @@ def test_psk_srrc():
     s = rng.integers(0, psk.order, 1_000)  # Signal is 1000 seconds long
     x = psk.modulate(s)
     t_rms = sdr.rms_integration_time(x, sample_rate=psk.sps)
-    assert t_rms == pytest.approx(288.66026000704875)
+    assert t_rms == pytest.approx(288.66026000704875, 1e-3)
 
 
 def test_psk_srrc_parabolic():
@@ -30,4 +30,4 @@ def test_psk_srrc_parabolic():
     y = x * np.linspace(-1, 1, len(x)) ** 2  # Parabolic pulse shape
     y *= np.sqrt(sdr.energy(x) / sdr.energy(y))  # Normalize energy
     t_rms = sdr.rms_integration_time(y, sample_rate=psk.sps)
-    assert t_rms == pytest.approx(422.61669635555705)
+    assert t_rms == pytest.approx(422.61669635555705, 1e-3)
