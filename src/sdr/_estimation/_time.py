09c6f52c3be01b1e29abe51262b1910f4f0db23f
mhostetter <matthostetter@gmail.com>
2024-05-27 11:53:20 -0400

    Add `sdr.crlb_toa()`
    Fixes #334

--- src/sdr/_estimation/_time.py
+++ src/sdr/_estimation/_time.py
@@ -13,6 +13,84 @@ from ._snr import composite_snr
 
 
 @export
+def toa_crlb(
+    snr: npt.ArrayLike,
+    time: npt.ArrayLike,
+    bandwidth: npt.ArrayLike,
+    rms_bandwidth: npt.ArrayLike | None = None,
+    noise_bandwidth: npt.ArrayLike | None = None,
+) -> npt.NDArray[np.float64]:
+    r"""
+    Calculates the Cramér-Rao lower bound (CRLB) on time of arrival (TOA) estimation.
+
+    Arguments:
+        snr: The signal-to-noise ratio (SNR) of the signal $\gamma = S / (N_0 B_n)$ in dB.
+        time: The integration time $T$ in seconds.
+        bandwidth: The signal bandwidth $B_s$ in Hz.
+        rms_bandwidth: The root-mean-square (RMS) bandwidth $B_{s,\text{rms}}$ in Hz. If `None`, the RMS bandwidth
+            is calculated assuming a rectangular spectrum, $B_{s,\text{rms}} = B_s/\sqrt{12}$.
+        noise_bandwidth: The noise bandwidth $B_n$ in Hz. If `None`, the noise bandwidth is assumed to be the
+            signal bandwidth $B_s$. The noise bandwidth must be the same for both signals.
+
+    Returns:
+        The Cramér-Rao lower bound (CRLB) on the time of arrival (TOA) estimation error standard deviation
+        $\sigma_{\text{toa}}$ in seconds.
+
+    See Also:
+        sdr.rms_bandwidth
+
+    Notes:
+        The Cramér-Rao lower bound (CRLB) on the time of arrival (TOA) estimation error standard deviation
+        $\sigma_{\text{toa}}$ is given by
+
+        $$\sigma_{\text{toa}} = \frac{1}{\pi \sqrt{8} B_{s,\text{rms}}} \frac{1}{\sqrt{B_n T \gamma}}$$
+
+        $$B_{s,\text{rms}} = \sqrt{\frac{\int_{-\infty}^{\infty} (f - \mu_f)^2 S(f - \mu_f) \, df}{\int_{-\infty}^{\infty} S(f - \mu_f) \, df}}$$
+
+        where $\gamma$ is the signal-to-noise ratio (SNR), $S(f)$ is the power spectral density (PSD) of the signal,
+        and $\mu_f$ is the centroid of the PSD.
+
+        .. note::
+            The constant terms from Stein's original equations were rearranged. The factor of 2 was removed from
+            $\gamma$ and the factor of $2\pi$ was removed from $B_{s,\text{rms}}$ and incorporated into the CRLB
+            equation.
+
+        The signal-to-noise ratio (SNR) $\gamma$ is improved by the coherent integration gain, which is the
+        time-bandwidth product $B_n T$. The product $B_n T \gamma$ is the output SNR of the matched filter
+        or correlator, which is equivalent to $E / N_0$.
+
+        $$B_n T \gamma = B_n T \frac{S}{N_0 B_n} = \frac{S T}{N_0} = \frac{E}{N_0}$$
+
+        The time measurement precision is inversely proportional to the bandwidth of the signal and the square root of
+        the output SNR.
+
+    Examples:
+        .. ipython:: python
+
+            snr = 10
+            time = np.logspace(-6, 0, 101)
+
+            @savefig sdr_toa_crlb_1.png
+            plt.figure(); \
+            plt.loglog(time, sdr.toa_crlb(snr, time, 1e5), label="100 kHz"); \
+            plt.loglog(time, sdr.toa_crlb(snr, time, 1e6), label="1 MHz"); \
+            plt.loglog(time, sdr.toa_crlb(snr, time, 1e7), label="10 MHz"); \
+            plt.loglog(time, sdr.toa_crlb(snr, time, 1e8), label="100 MHz"); \
+            plt.legend(title="Bandwidth"); \
+            plt.xlim(1e-6, 1e0); \
+            plt.ylim(1e-12, 1e-6); \
+            plt.xlabel("Integration time (s), $T$"); \
+            plt.ylabel(r"CRLB on TOA (s), $\sigma_{\text{toa}}$"); \
+            plt.title(f"Cramér-Rao lower bound (CRLB) on TOA estimation error\nstandard deviation with {snr}-dB SNR");
+
+    Group:
+        estimation-time
+    """
+    # The second signal's SNR of 1 million dB is equivalent to a noiseless template
+    return tdoa_crlb(snr, 1_000_000, time, bandwidth, rms_bandwidth, noise_bandwidth)
+
+
+@export
 def tdoa_crlb(
     snr1: npt.ArrayLike,
     snr2: npt.ArrayLike,
