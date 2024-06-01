bf4a6d82e5a4501eeed4fd00995b20709027764a
mhostetter <matthostetter@gmail.com>
2024-06-01 09:28:02 -0400

    Add `ax` kwarg to all plotting functions
    Fixes #340

--- src/sdr/plot/_detection.py
+++ src/sdr/plot/_detection.py
@@ -19,6 +19,7 @@ def p_d(
     x: npt.ArrayLike,
     p_d: npt.ArrayLike,
     x_label: Literal["snr", "enr"] = "snr",
+    ax: plt.Axes | None = None,
     **kwargs,
 ):
     r"""
@@ -28,29 +29,33 @@ def p_d(
         x: The SNR or ENR in dB.
         p_d: The probability of detection $P_d$.
         x_label: The x-axis label to use.
+        ax: The axis to plot on. If `None`, the current axis is used.
         kwargs: Additional keyword arguments to pass to :func:`matplotlib.pyplot.plot()`.
 
     Group:
         plot-detection
     """
     with plt.rc_context(RC_PARAMS):
+        if ax is None:
+            ax = plt.gca()
+
         default_kwargs = {}
         kwargs = {**default_kwargs, **kwargs}
 
-        plt.plot(x, p_d, **kwargs)
+        ax.plot(x, p_d, **kwargs)
 
         if "label" in kwargs:
-            plt.legend()
+            ax.legend()
 
         if x_label == "snr":
-            plt.xlabel(r"Signal-to-noise ratio (dB), $S/\sigma^2$")
+            ax.set_xlabel(r"Signal-to-noise ratio (dB), $S/\sigma^2$")
         elif x_label == "enr":
-            plt.xlabel(r"Energy-to-noise ratio (dB), $\mathcal{E}/\sigma^2$")
+            ax.set_xlabel(r"Energy-to-noise ratio (dB), $\mathcal{E}/\sigma^2$")
         else:
             raise ValueError(f"Argument 'x_label' must be one of ['snr', 'enr'], not {x_label!r}.")
 
-        plt.ylabel("Probability of detection, $P_d$")
-        plt.title("Detection performance")
+        ax.set_ylabel("Probability of detection, $P_d$")
+        ax.set_title("Detection performance")
 
 
 @export
@@ -58,6 +63,7 @@ def roc(
     p_fa: npt.ArrayLike,
     p_d: npt.ArrayLike,
     type: Literal["linear", "semilogx", "semilogy", "loglog"] = "semilogx",
+    ax: plt.Axes | None = None,
     **kwargs,
 ):
     r"""
@@ -67,34 +73,38 @@ def roc(
         p_fa: The probability of false alarm $P_{fa}$.
         p_d: The probability of detection $P_d$.
         type: The type of plot to generate.
+        ax: The axis to plot on. If `None`, the current axis is used.
         kwargs: Additional keyword arguments to pass to the plotting function defined by `type`.
 
     Group:
         plot-detection
     """
     with plt.rc_context(RC_PARAMS):
+        if ax is None:
+            ax = plt.gca()
+
         default_kwargs = {}
         kwargs = {**default_kwargs, **kwargs}
 
         if type == "linear":
-            plt.plot(p_fa, p_d, **kwargs)
+            ax.plot(p_fa, p_d, **kwargs)
         elif type == "semilogx":
-            plt.semilogx(p_fa, p_d, **kwargs)
+            ax.semilogx(p_fa, p_d, **kwargs)
         elif type == "semilogy":
-            plt.semilogy(p_fa, p_d, **kwargs)
+            ax.semilogy(p_fa, p_d, **kwargs)
         elif type == "loglog":
-            plt.loglog(p_fa, p_d, **kwargs)
+            ax.loglog(p_fa, p_d, **kwargs)
         else:
             raise ValueError(
                 f"Argument 'type' must be one of ['linear', 'semilogx', 'semilogy', 'loglog'], not {type!r}."
             )
 
         if "label" in kwargs:
-            plt.legend()
+            ax.legend()
 
-        plt.xlabel("Probability of false alarm, $P_{fa}$")
-        plt.ylabel("Probability of detection, $P_d$")
-        plt.title("Receiver operating characteristic (ROC) curve")
+        ax.set_xlabel("Probability of false alarm, $P_{fa}$")
+        ax.set_ylabel("Probability of detection, $P_d$")
+        ax.set_title("Receiver operating characteristic (ROC) curve")
 
 
 @export
@@ -108,6 +118,7 @@ def detector_pdfs(
     points: int = 1001,
     p_h0: float = 1e-6,
     p_h1: float = 1e-3,
+    ax: plt.Axes | None = None,
     **kwargs,
 ):
     r"""
@@ -123,6 +134,7 @@ def detector_pdfs(
         points: The number of points to use for the x-axis.
         p_h0: The probability of the $\mathcal{H}_0$ tails to plot. The smaller the value, the longer the x-axis.
         p_h1: The probability of the $\mathcal{H}_1$ tails to plot. The smaller the value, the longer the x-axis.
+        ax: The axis to plot on. If `None`, the current axis is used.
         kwargs: Additional keyword arguments to pass to :func:`matplotlib.pyplot.plot()`.
 
     See Also:
@@ -151,6 +163,9 @@ def detector_pdfs(
         plot-detection
     """
     with plt.rc_context(RC_PARAMS):
+        if ax is None:
+            ax = plt.gca()
+
         default_kwargs = {}
         kwargs = {**default_kwargs, **kwargs}
 
@@ -176,23 +191,23 @@ def detector_pdfs(
             x = np.linspace(x_min, x_max, points)
 
         if h0 is not None:
-            plt.plot(x, h0.pdf(x), label=r"$\mathcal{H}_0$: Noise", **kwargs)
+            ax.plot(x, h0.pdf(x), label=r"$\mathcal{H}_0$: Noise", **kwargs)
             if shade and threshold:
-                h0_color = plt.gca().lines[-1].get_color()
-                plt.fill_between(x, 0, h0.pdf(x), where=(x >= threshold), interpolate=True, color=h0_color, alpha=0.1)
+                h0_color = ax.lines[-1].get_color()
+                ax.fill_between(x, 0, h0.pdf(x), where=(x >= threshold), interpolate=True, color=h0_color, alpha=0.1)
         if h1 is not None:
-            plt.plot(x, h1.pdf(x), label=r"$\mathcal{H}_1$: Signal + Noise", **kwargs)
+            ax.plot(x, h1.pdf(x), label=r"$\mathcal{H}_1$: Signal + Noise", **kwargs)
             if shade and threshold:
-                h1_color = plt.gca().lines[-1].get_color()
-                plt.fill_between(x, 0, h1.pdf(x), where=(x >= threshold), interpolate=True, color=h1_color, alpha=0.1)
+                h1_color = ax.lines[-1].get_color()
+                ax.fill_between(x, 0, h1.pdf(x), where=(x >= threshold), interpolate=True, color=h1_color, alpha=0.1)
         if threshold is not None:
-            plt.axvline(threshold, color="k", linestyle="--", label="Threshold")
+            ax.axvline(threshold, color="k", linestyle="--", label="Threshold")
 
         if annotate:
             if h0 is not None and threshold is not None:
                 p_fa = h0.sf(threshold)
                 threshold_half = h0.isf(p_fa / 2)
-                plt.text(
+                ax.text(
                     threshold_half,
                     h0.pdf(threshold_half) / 2,
                     rf"$P_{{fa}} = $ {p_fa:1.2e}",
@@ -204,7 +219,7 @@ def detector_pdfs(
             if h1 is not None and threshold is not None:
                 p_d = h1.sf(threshold)
                 threshold_half = h1.isf(p_d / 2)
-                plt.text(
+                ax.text(
                     threshold_half,
                     h1.pdf(threshold_half) / 2,
                     rf"$P_{{d}} = $ {p_d:1.2e}",
@@ -213,7 +228,7 @@ def detector_pdfs(
                     va="center",
                 )
 
-        plt.legend()
-        plt.xlabel("Test statistic, $T(x)$")
-        plt.ylabel("Probability density")
-        plt.title("Detector probability density functions (PDFs)")
+        ax.legend()
+        ax.set_xlabel("Test statistic, $T(x)$")
+        ax.set_ylabel("Probability density")
+        ax.set_title("Detector probability density functions (PDFs)")
--- src/sdr/plot/_helper.py
+++ src/sdr/plot/_helper.py
@@ -7,6 +7,7 @@ from typing_extensions import Literal
 
 
 def real_or_complex_plot(
+    ax: plt.Axes,
     t: npt.NDArray,
     x: npt.NDArray,
     diff: Literal["color", "line"] = "color",
@@ -20,19 +21,19 @@ def real_or_complex_plot(
             label, label2 = label + " (real)", label + " (imag)"
 
         if diff == "color":
-            plt.plot(t, x.real, label=label, **kwargs)
-            plt.plot(t, x.imag, label=label2, **kwargs)
+            ax.plot(t, x.real, label=label, **kwargs)
+            ax.plot(t, x.imag, label=label2, **kwargs)
         elif diff == "line":
-            (real,) = plt.plot(t, x.real, "-", label=label, **kwargs)
+            (real,) = ax.plot(t, x.real, "-", label=label, **kwargs)
             kwargs.pop("color", None)
-            plt.plot(t, x.imag, "--", color=real.get_color(), label=label2, **kwargs)
+            ax.plot(t, x.imag, "--", color=real.get_color(), label=label2, **kwargs)
         else:
             raise ValueError(f"Argument 'diff' must be 'color' or 'line', not {diff}.")
     else:
-        plt.plot(t, x, label=label, **kwargs)
+        ax.plot(t, x, label=label, **kwargs)
 
     if label:
-        plt.legend()
+        ax.legend()
 
 
 def min_ylim(y: npt.NDArray, separation: float, sample_rate: float):
