2ad628fb4d262fe400a79abe238beb9b4e5af478
mhostetter <matthostetter@gmail.com>
2024-06-13 17:52:58 -0400

    Add unit tests for `preferred_pairs()`

---
+++ tests/sequences/test_preferred_pairs.py
@@ -0,0 +1,61 @@
+import galois
+import pytest
+
+import sdr
+
+
+def test_exceptions():
+    with pytest.raises(TypeError):
+        # Degree must be an integer
+        sdr.preferred_pairs(6.0)
+    with pytest.raises(ValueError):
+        # Degree must be positive
+        sdr.preferred_pairs(-1)
+
+    with pytest.raises(TypeError):
+        # Polynomial must be polynomial like
+        sdr.preferred_pairs(6, poly1=1.0)
+    with pytest.raises(ValueError):
+        # Polynomial must be a primitive polynomial
+        sdr.preferred_pairs(6, poly1=galois.Poly([1, 0, 0, 0, 0, 0, 1]))
+    with pytest.raises(ValueError):
+        # Polynomial must have correct degree
+        sdr.preferred_pairs(6, poly1=galois.Poly.Degrees([4, 1, 0]))
+
+
+def test_degree_divides_4():
+    assert list(sdr.preferred_pairs(4)) == []
+    assert list(sdr.preferred_pairs(8)) == []
+    assert list(sdr.preferred_pairs(12)) == []
+
+
+def test_degree_3():
+    assert list(sdr.preferred_pairs(3)) == [
+        (galois.Poly.Str("x^3 + x + 1"), galois.Poly.Str("x^3 + x^2 + 1")),
+    ]
+
+
+def test_degree_5():
+    assert list(sdr.preferred_pairs(5)) == [
+        (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^3 + x^2 + x + 1")),
+        (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^4 + x^2 + x + 1")),
+        (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x + 1")),
+        (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x^2 + 1")),
+        (galois.Poly.Str("x^5 + x^3 + 1"), galois.Poly.Str("x^5 + x^3 + x^2 + x + 1")),
+        (galois.Poly.Str("x^5 + x^3 + 1"), galois.Poly.Str("x^5 + x^4 + x^2 + x + 1")),
+        (galois.Poly.Str("x^5 + x^3 + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x + 1")),
+        (galois.Poly.Str("x^5 + x^3 + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x^2 + 1")),
+        (galois.Poly.Str("x^5 + x^3 + x^2 + x + 1"), galois.Poly.Str("x^5 + x^4 + x^2 + x + 1")),
+        (galois.Poly.Str("x^5 + x^3 + x^2 + x + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x + 1")),
+        (galois.Poly.Str("x^5 + x^4 + x^2 + x + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x^2 + 1")),
+        (galois.Poly.Str("x^5 + x^4 + x^3 + x + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x^2 + 1")),
+    ]
+
+
+def test_degree_5_specific():
+    assert list(sdr.preferred_pairs(5, poly1="x^5 + x^2 + 1")) == [
+        (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^3 + x^2 + x + 1")),
+        (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^4 + x^2 + x + 1")),
+        (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x + 1")),
+        (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x^2 + 1")),
+    ]
