60daa1ab64c6c6118e1a23d03eb538f3730c5189
mhostetter <matthostetter@gmail.com>
2024-06-15 10:31:47 -0400

    Move `stem()` to separate file

--- src/sdr/plot/__init__.py
+++ src/sdr/plot/__init__.py
@@ -4,8 +4,8 @@ A subpackage for various plotting functions using :obj:`matplotlib`.
 
 from ._detection import *
 from ._filter import *
-from ._helper import *
 from ._modulation import *
 from ._rc_params import *
 from ._spectral_estimation import *
 from ._time_domain import *
+from ._utility import *
