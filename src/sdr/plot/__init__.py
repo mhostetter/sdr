5a118dc2ba72a72b896283b997d5908e76b62a7e
mhostetter <matthostetter@gmail.com>
2024-06-15 12:34:29 -0400

    Add `sdr.plot.fft`
    Fixes #19

--- src/sdr/plot/__init__.py
+++ src/sdr/plot/__init__.py
@@ -4,6 +4,7 @@ A subpackage for various plotting functions using :obj:`matplotlib`.
 
 from ._detection import *
 from ._filter import *
+from ._freq_domain import *
 from ._modulation import *
 from ._rc_params import *
 from ._spectral_estimation import *
