4efbaf3fcae771f96d16467cdbcb1ece4b149e0f
mhostetter <matthostetter@gmail.com>
2024-07-05 17:21:01 -0400

    Rename `poly1` to `poly` in `preferred_pairs()`

--- tests/sequences/test_preferred_pairs.py
+++ tests/sequences/test_preferred_pairs.py
@@ -15,13 +15,13 @@ def test_exceptions():
 
     with pytest.raises(TypeError):
         # Polynomial must be polynomial like
-        next(sdr.preferred_pairs(6, poly1=1.0))
+        next(sdr.preferred_pairs(6, poly=1.0))
     with pytest.raises(ValueError):
         # Polynomial must be a primitive polynomial
-        next(sdr.preferred_pairs(6, poly1=galois.Poly([1, 0, 0, 0, 0, 0, 1])))
+        next(sdr.preferred_pairs(6, poly=galois.Poly([1, 0, 0, 0, 0, 0, 1])))
     with pytest.raises(ValueError):
         # Polynomial must have correct degree
-        next(sdr.preferred_pairs(6, poly1=galois.Poly.Degrees([4, 1, 0])))
+        next(sdr.preferred_pairs(6, poly=galois.Poly.Degrees([4, 1, 0])))
 
 
 def test_degree_divides_4():
@@ -54,7 +54,7 @@ def test_degree_5():
 
 
 def test_degree_5_specific():
-    assert list(sdr.preferred_pairs(5, poly1="x^5 + x^2 + 1")) == [
+    assert list(sdr.preferred_pairs(5, poly="x^5 + x^2 + 1")) == [
         (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^3 + x^2 + x + 1")),
         (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^4 + x^2 + x + 1")),
         (galois.Poly.Str("x^5 + x^2 + 1"), galois.Poly.Str("x^5 + x^4 + x^3 + x + 1")),
