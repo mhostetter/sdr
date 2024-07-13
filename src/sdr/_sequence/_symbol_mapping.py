e1689f8843d6e21cdd89358ff99646fcb1126421
mhostetter <matthostetter@gmail.com>
2024-07-12 20:00:08 -0400

    Rework `binary_code()` and `gray_code()`
    Fixes #388

--- src/sdr/_sequence/_symbol_mapping.py
+++ src/sdr/_sequence/_symbol_mapping.py
@@ -11,12 +11,12 @@ from .._helper import export, verify_scalar
 
 
 @export
-def binary_code(degree: int) -> npt.NDArray[np.int_]:
+def binary_code(length: int) -> npt.NDArray[np.int_]:
     """
     Generates a binary code of length $n = 2^m$.
 
     Arguments:
-        degree: The degree $m$ of the binary code.
+        length: The length $n = 2^m$ of the binary code.
 
     Returns:
         A binary code of length $n = 2^m$.
@@ -24,26 +24,26 @@ def binary_code(degree: int) -> npt.NDArray[np.int_]:
     Examples:
         .. ipython:: python
 
-            sdr.binary_code(1)
             sdr.binary_code(2)
-            sdr.binary_code(3)
             sdr.binary_code(4)
+            sdr.binary_code(8)
+            sdr.binary_code(16)
 
     Group:
         sequences-symbol-mapping
     """
-    verify_scalar(degree, int=True, positive=True)
+    verify_scalar(length, int=True, positive=True, power_of_two=True)
 
-    return np.arange(2**degree)
+    return np.arange(length)
 
 
 @export
-def gray_code(degree: int) -> npt.NDArray[np.int_]:
+def gray_code(length: int) -> npt.NDArray[np.int_]:
     """
     Generates a Gray code of length $n = 2^m$.
 
     Arguments:
-        degree: The degree $m$ of the Gray code.
+        length: The length $n = 2^m$ of the Gray code.
 
     Returns:
         A Gray code of length $n = 2^m$.
@@ -51,23 +51,23 @@ def gray_code(degree: int) -> npt.NDArray[np.int_]:
     Examples:
         .. ipython:: python
 
-            sdr.gray_code(1)
             sdr.gray_code(2)
-            sdr.gray_code(3)
             sdr.gray_code(4)
+            sdr.gray_code(8)
+            sdr.gray_code(16)
 
     Group:
         sequences-symbol-mapping
     """
-    verify_scalar(degree, int=True, positive=True)
+    verify_scalar(length, int=True, positive=True, power_of_two=True)
 
-    if degree == 1:
+    if length == 2:
         return np.array([0, 1])
 
     # Generate the Gray code with degree m - 1
-    code_1 = gray_code(degree - 1)
+    code_1 = gray_code(length // 2)
 
     # Generate the Gray code for degree m by concatenating the Gray code for degree m - 1
     # with itself reversed. Also, the most significant bit of the second half
     # is set to 1.
-    return np.concatenate((code_1, code_1[::-1] + 2 ** (degree - 1)))
+    return np.concatenate((code_1, code_1[::-1] + length // 2))
--- tests/modulation/test_binary_code.py
+++ tests/modulation/test_binary_code.py
@@ -4,28 +4,28 @@ import sdr
 
 
 def test_1():
-    code = sdr.binary_code(1)
+    code = sdr.binary_code(2)
     code_truth = [0, 1]
     assert isinstance(code, np.ndarray)
     assert np.array_equal(code, code_truth)
 
 
 def test_2():
-    code = sdr.binary_code(2)
+    code = sdr.binary_code(4)
     code_truth = [0, 1, 2, 3]
     assert isinstance(code, np.ndarray)
     assert np.array_equal(code, code_truth)
 
 
 def test_3():
-    code = sdr.binary_code(3)
+    code = sdr.binary_code(8)
     code_truth = [0, 1, 2, 3, 4, 5, 6, 7]
     assert isinstance(code, np.ndarray)
     assert np.array_equal(code, code_truth)
 
 
 def test_4():
-    code = sdr.binary_code(4)
+    code = sdr.binary_code(16)
     code_truth = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     assert isinstance(code, np.ndarray)
     assert np.array_equal(code, code_truth)
