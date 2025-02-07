4540b9bf5fa02f84a255493bf7291dc1d05606be
mhostetter <matthostetter@gmail.com>
2025-02-07 18:47:37 -0500

    Add unit tests for `local_oscillator()`

---
+++ tests/dsp/test_local_oscillator.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+import sdr
+
+
+def test_complex():
+    sample_rate = 1e3  # samples/s
+    N = 100  # samples
+    freq = 10  # Hz
+    phase = 45  # degrees
+    lo = sdr.local_oscillator(N / sample_rate, freq=freq, phase=phase, sample_rate=sample_rate, complex=True)
+    lo_truth = np.exp(1j * (2 * np.pi * freq / sample_rate * np.arange(N) + np.deg2rad(phase)))
+    assert np.allclose(lo, lo_truth)
+
+
+def test_real():
+    sample_rate = 1e3  # samples/s
+    N = 100  # samples
+    freq = 10  # Hz
+    phase = 45  # degrees
+    lo = sdr.local_oscillator(N / sample_rate, freq=freq, phase=phase, sample_rate=sample_rate, complex=False)
+    lo_truth = np.cos(2 * np.pi * freq / sample_rate * np.arange(N) + np.deg2rad(phase))
+    assert np.allclose(lo, lo_truth)
